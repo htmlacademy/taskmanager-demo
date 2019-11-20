bb617886ba993b50bc5b5076f498e11e16f8e93a
kicumkicum <kicumkicum@gmail.com>
2019-11-20 17:04:49 +0300

    :hatching_chick: Начальное состояние проекта

---
+++ requirements.md
@@ -0,0 +1,30 @@
+# Список внешних зависимостей
+
+Ниже представлен полный список пакетов, которые используются для разработки проекта «Менеджер задач». Обратите внимание, все пакеты должны быть установлены определённой версии. Версия пакета вводится после символа `@`. Например:
+
+```bash
+npm i <package>@<version> -E
+npm i dayjs@1.11.1 -E
+npm i eslint@8.13.0 -DE
+```
+
+## Зависимости приложения (`dependencies`)
+
+- dayjs@1.11.1
+- flatpickr@4.6.13
+- he@1.2.0
+
+## Зависимости для разработки (`devDependencies`)
+
+- eslint@8.13.0
+- eslint-config-htmlacademy@5.1.2
+- webpack@5.72.0
+- webpack-cli@4.9.2
+- webpack-dev-server@4.8.1
+- copy-webpack-plugin@10.2.4
+- @babel/core@7.17.9
+- @babel/preset-env@7.16.11
+- babel-loader@8.2.4
+- css-loader@6.7.1
+- style-loader@3.3.1
+
---
+++ src/render.js
@@ -0,0 +1,41 @@
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+// Принцип работы прост:
+// 1. создаём пустой div-блок
+// 2. берём HTML в виде строки и вкладываем в этот div-блок, превращая в DOM-элемент
+// 3. возвращаем этот DOM-элемент
+const createElement = (template) => {
+  const newElement = document.createElement('div'); // 1
+  newElement.innerHTML = template; // 2
+
+  return newElement.firstElementChild; // 3
+};
+// Единственный нюанс, что HTML в строке должен иметь общую обёртку,
+// то есть быть чем-то вроде <nav><a>Link 1</a><a>Link 2</a></nav>,
+// а не просто <a>Link 1</a><a>Link 2</a>
+
+const render = (component, container, place = RenderPosition.BEFOREEND) => {
+  const element = component.getElement();
+
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+export {RenderPosition, createElement, render};
