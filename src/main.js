c9339b65ae56bcac00b50c3e4b09c8baa6376da2
Sasha Sushko <sushko@outlook.com>
2020-04-02 14:33:02 +0300

    2.5 Импортирует компоненты в точку входа

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import {createSiteMenuTemplate} from "./components/site-menu.js";
+import {createSortingTemplate} from "./components/sorting";
+import {createFilterTemplate} from "./components/filter.js";
+import {createTaskTemplate} from "./components/task.js";
+import {createTaskEditTemplate} from "./components/task-edit.js";
+import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
+import {createBoardTemplate} from "./components/board.js";
+
 const TASK_COUNT = 3;
 
 const render = (container, template, place) => {
