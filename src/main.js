d3f389fd012c74b9b33a2563822b98d5000bf053
kicumkicum <kicumkicum@gmail.com>
2020-04-06 01:02:54 +0300

    3.3.3 Добавит пустой обработчик кнопки load-more

--- src/main.js
+++ src/main.js
@@ -40,3 +40,9 @@ for (let i = 1; i < showingTasksCount; i++) {
 }
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+loadMoreButton.addEventListener(`click`, () => {
+
+});
