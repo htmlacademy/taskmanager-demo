b2257d3e1fd653e22c680de30d1c114825cd8c20
Sasha Sushko <sushko@outlook.com>
2020-04-13 10:34:08 +0300

    4.1.4 Рефакторинг. WIP
    - Переименует компонент сортировки, чтобы не нарушать критерий о наименовании
    - Выделит из доски задач компонент списка задач, потому что на доске у нас будет и сортировка, и кнопка «Load more», и кто знает что ещё

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,20 +1,19 @@
 import {createElement} from "../utils.js";
 
-
-const createBoardTemplate = () => {
+const createTasksTemplate = () => {
   return (
-    `<section class="board container"></section>`
+    `<div class="board__tasks"></div>`
   );
 };
 
 
-export default class Board {
+export default class Tasks {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createBoardTemplate();
+    return createTasksTemplate();
   }
 
   getElement() {
