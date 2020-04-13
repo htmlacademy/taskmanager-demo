4eec6f824c407c75697a7078b4cfd244ee522f0e
Sasha Sushko <sushko@outlook.com>
2020-04-13 10:17:16 +0300

    4.1.1 Добавит вспомогательную функцию для создания DOM-элемента

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,10 @@ export const formatTime = (date) => {
 
   return `${hours}:${minutes}`;
 };
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
