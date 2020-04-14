20f902e2050c34a6bdc64db4ab3cb1019965bd22
kicumkicum <kicumkicum@gmail.com>
2020-04-14 19:03:36 +0300

    5.1.4 Унаследует компонент `board` от абстрактного компонента
    При наследовании у нас сильно сократилось количество кода в компоненте.
    Потребовалось удалить все то, что полностью реализовано в абстракном классе:
    `getElement` и `removeElement`. Осталась лишь реализация метода `getTemplate`.
    Напомню, что метод `getTemplate` должен быть обязательно реализован. Иначе
    будет выброшено исключение, описанное в абстрактном `getTemplate`.

--- src/components/board.js
+++ src/components/board.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils.js";
+import AbstractComponent from "./abstract-component.js";
 
 
 const createBoardTemplate = () => {
@@ -8,24 +8,8 @@ const createBoardTemplate = () => {
 };
 
 
-export default class Board {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Board extends AbstractComponent {
   getTemplate() {
     return createBoardTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
