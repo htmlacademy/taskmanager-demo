59314ffbc577b015edf217344eb7d3b047f133fd
kicumkicum <kicumkicum@gmail.com>
2020-04-14 18:53:03 +0300

    5.1.3 Добавит абстрактный метод `getTemplate`
    В компонентах есть метод с общим именем, но различной реализацией - `getTemplate`.
    Добавим его в абстрактный класс, но его реализация в нем будет бросать исключение.
    Это значит, что его потребуется переопределить в классах-наследниках.

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -9,6 +9,10 @@ export default class AbstractComponent {
     this._element = null;
   }
 
+  getTemplate() {
+    throw new Error(`Abstract method not implemented: getTemplate`);
+  }
+
   getElement() {
     if (!this._element) {
       this._element = createElement(this.getTemplate());
