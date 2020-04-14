c54c610743e336141ebfdd6599594c4bd43d8052
kicumkicum <kicumkicum@gmail.com>
2020-04-14 18:50:20 +0300

    5.1.2 Переносит одинаковую реализацию общих для всех компонентов методов `getElement`, `removeElement`
    Посмотрев на все компоненты выделяем одинаковые для всех методы. Ими оказываются `getElement`, `removeElement`. Так же проинициализируем в конструкторе свойство `_element`.

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,7 +1,23 @@
+import {createElement} from "../utils.js";
+
 export default class AbstractComponent {
   constructor() {
     if (new.target === AbstractComponent) {
       throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
     }
+
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
