4c60ef3f824dac86ce4104d11bd53ce82a2701c0
Sasha Sushko <sushko@outlook.com>
2020-04-27 12:19:36 +0300

    7.1.5 Научит модель учитывать выбранный фильтр

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -19,7 +19,7 @@ export default class FilterController {
 
   render() {
     const container = this._container;
-    const allTasks = this._tasksModel.getTasks();
+    const allTasks = this._tasksModel.getTasksAll();
     const filters = Object.values(FilterType).map((filterType) => {
       return {
         name: filterType,
@@ -40,6 +40,7 @@ export default class FilterController {
   }
 
   _onFilterChange(filterType) {
+    this._tasksModel.setFilter(filterType);
     this._activeFilterType = filterType;
   }
 
--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,17 +1,32 @@
+import {getTasksByFilter} from "../utils/filter.js";
+import {FilterType} from "../const.js";
+
+
 export default class Tasks {
   constructor() {
     this._tasks = [];
+    this._activeFilterType = FilterType.ALL;
 
     this._dataChangeHandlers = [];
+    this._filterChangeHandlers = [];
   }
 
   getTasks() {
+    return getTasksByFilter(this._tasks, this._activeFilterType);
+  }
+
+  getTasksAll() {
     return this._tasks;
   }
 
   setTasks(tasks) {
     this._tasks = Array.from(tasks);
     this._callHandlers(this._dataChangeHandlers);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._callHandlers(this._filterChangeHandlers);
   }
 
   updateTask(id, task) {
