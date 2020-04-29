b2f3b8cd8de8dde66966096a571a47e28b0f0aaa
kicumkicum <kicumkicum@gmail.com>
2020-04-29 19:36:29 +0300

    8.2.2 Бросает исключение, если ответ сервера отличается от 200
    Теперь мы поймаем исключение в месте проявления проблемы.

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import Task from "./models/task.js";
 
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 const API = class {
   constructor(authorization) {
     this._authorization = authorization;
@@ -10,6 +18,7 @@ const API = class {
     headers.append(`Authorization`, this._authorization);
 
     return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks`, {headers})
+      .then(checkStatus)
       .then((response) => response.json())
       .then(Task.parseTasks);
   }
@@ -23,6 +32,7 @@ const API = class {
       body: JSON.stringify(data),
       headers,
     })
+      .then(checkStatus)
       .then((response) => response.json())
       .then(Task.parseTasks);
   }
