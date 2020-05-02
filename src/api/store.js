7b0de63f4ec7fa29d2cc8c73fea4a96feb6b7c3d
kicumkicum <kicumkicum@gmail.com>
2020-05-02 17:37:00 +0300

    9.1.5 Добавит реализацую методов `getItems` и `setItem` в `store`
    Наш `store` заработал и можем вернуться к реализации осальных методов провайдера.

--- src/api/store.js
+++ src/api/store.js
@@ -1,10 +1,28 @@
 export default class Store {
+  constructor(key, storage) {
+    this._storage = storage;
+    this._storeKey = key;
+  }
+
   getItems() {
-    return {};
+    try {
+      return JSON.parse(this._storage.getItem(this._storeKey)) || {};
+    } catch (err) {
+      return {};
+    }
   }
 
   setItem(key, value) {
+    const store = this.getItems();
 
+    this._storage.setItem(
+        this._storeKey,
+        JSON.stringify(
+            Object.assign({}, store, {
+              [key]: value
+            })
+        )
+    );
   }
 
   removeItem(key) {
--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ import {render, RenderPosition} from "./utils/render.js";
 
 const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=`;
 const END_POINT = `https://11.ecmascript.pages.academy/task-manager`;
+const STORE_PREFIX = `taskmanager-localstorage`;
+const STORE_VER = `v1`;
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -20,7 +23,7 @@ const dateFrom = (() => {
 })();
 
 const api = new API(END_POINT, AUTHORIZATION);
-const store = new Store();
+const store = new Store(STORE_NAME, window.localStorage);
 const apiWithProvider = new Provider(api, store);
 const tasksModel = new TasksModel();
 
