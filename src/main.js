eb2cbb40ba78d36859b2ea9f6a34b63d65f6eea8
Sasha Sushko <sushko@outlook.com>
2020-07-27 12:39:02 +0300

    1.4 Пробуем отрендерить меню на страницу
    - Найдем контейнер siteHeaderElement для вставки меню
    - С помощью render отрисуем сгенерированную функцией createSiteMenuTemplate разметку в нужный контейнер.
    - Откроем public/index.html в браузере, чтобы проверить, что меню появилось на странице

--- src/main.js
+++ src/main.js
@@ -36,3 +36,8 @@ const createSiteMenuTemplate = () => {
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
