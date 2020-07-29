597aefc9e095f03fdb81b487592a5238f3ea2d06
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:14:26 +0300

    1.1 Установит и настроит сборщик (webpack)
    - Установим webpack, webpack-cli (команды терминала) и copy-webpack-plugin (плагин для копирования статики) из npm
    - Создадим файл настроек webpack
    - Укажем точку входа (entry) и выходные данные (output)
    - Опишем скрипт build для сборки файлов в бандл
    - Добавим собираемые файлы в Git-игнор
    - Добавим файл настроек webpack в игнор eslint
    
    N.B. Теперь, если выполнить в терминале команду npm run build, создастся директория build/, в которой окажутся все статические файлы из директории public/ и два файла: bundle.js и bundle.js.map
    Хранить собранные файлы в репозитории нет никакого смысла, потому что здесь же хранятся исходные файлы, и в случае необходимости можно всегда выполнить "npm run build"
    
    N.B. На самом деле последний пункт не обязателен, потому что eslint в проекте не следит за файлами вне директории "src", но, к сожалению, современные редакторы, вроде VS Code или WebStorm, оказывают медвежью услугу и проверяют все файлы в проекте, хотя их никто не просил. В том числе файл настроек webpack, где есть, например, require, который у нас под запретом

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+};
