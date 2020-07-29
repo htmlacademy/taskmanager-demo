7c61355f07b494f9febf0deae42fd3f6ac7b85bf
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:39:50 +0300

    1.4 Добавит модуль «Меню»
    - Создадим отдельный файл для функции по созданию шаблона меню
    - Импортируем этот файл в main.js и используем функцию createSiteMenuTemplate уже из него

---
+++ src/view/site-menu.js
@@ -0,0 +1,30 @@
+export const createSiteMenuTemplate = () => {
+  return `<section class="control__btn-wrap">
+    <input
+      type="radio"
+      name="control"
+      id="control__new-task"
+      class="control__input visually-hidden"
+    />
+    <label for="control__new-task" class="control__label control__label--new-task"
+      >+ ADD NEW TASK</label
+    >
+    <input
+      type="radio"
+      name="control"
+      id="control__task"
+      class="control__input visually-hidden"
+      checked
+    />
+    <label for="control__task" class="control__label">TASKS</label>
+    <input
+      type="radio"
+      name="control"
+      id="control__statistic"
+      class="control__input visually-hidden"
+    />
+    <label for="control__statistic" class="control__label"
+      >STATISTICS</label
+    >
+  </section>`;
+};
