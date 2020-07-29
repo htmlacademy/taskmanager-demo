61001ba457dc9fe93f0189dd87e14ba300d2bc64
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:42:57 +0300

    1.5 Разобьёт код на модули
    - Выделим остальные функции по созданию шаблонов в отдельные файлы
    - Импортируем эти функции в main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createFilterTemplate} from './view/filter.js';
+import {createTaskTemplate} from './view/task.js';
+import {createTaskEditTemplate} from './view/task-edit.js';
+import {createLoadMoreButtonTemplate} from './view/load-more-button.js';
+import {createBoardTemplate} from './view/board.js';
 import {renderTemplate, RenderPosition} from './render.js';
+
+const TASK_COUNT = 3;
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 renderTemplate(siteHeaderElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createFilterTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createBoardTemplate(), RenderPosition.BEFOREEND);
+
+const boardElement = siteMainElement.querySelector('.board');
+const taskListElement = boardElement.querySelector('.board__tasks');
+
+renderTemplate(taskListElement, createTaskEditTemplate(), RenderPosition.BEFOREEND);
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  renderTemplate(taskListElement, createTaskTemplate(), RenderPosition.BEFOREEND);
+}
+
+renderTemplate(boardElement, createLoadMoreButtonTemplate(), RenderPosition.BEFOREEND);
