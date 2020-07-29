83e3556f74b788b409275b0617d878aa96291f57
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:10:19 +0300

    1.2 Настроит dev-сервер и добавит собранные файлы в игнор
    - Установим пакет сервера webpack-dev-server
    - Опишем скрипт start для запуска сервера локально
    - Настроим сервер в файле настроек webpack
    - Добавим собираемые файлы в Git-игнор
    
    N.B. Теперь, если выполнить в терминале команду npm run start, запустится локальный сервер для разработки. По умолчанию он будет доступен по адресу http://localhost:8080
    
    N.B. Хранить собранные файлы в репозитории нет никакого смысла, потому что здесь же хранятся исходные файлы, и в случае необходимости можно всегда выполнить "npm run build"

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,8 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    hot: false
+  }
 };
