7bb4f7c3fad1fe908493cd912fe47659d3c9f6a4
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:42:57 +0300

    1.5 Разобьёт код на модули
    - Выделим остальные функции по созданию шаблонов в отдельные файлы
    - Импортируем эти функции в main.js

---
+++ src/view/board.js
@@ -0,0 +1,11 @@
+export const createBoardTemplate = () => (
+  `<section class="board container">
+    <div class="board__sort-list">
+      <a href="#" class="board__sort-item">SORT BY DEFAULT</a>
+      <a href="#" class="board__sort-item">SORT BY DATE up</a>
+      <a href="#" class="board__sort-item">SORT BY DATE down</a>
+    </div>
+
+    <div class="board__tasks"></div>
+  </section>`
+);
---
+++ src/view/load-more-button.js
@@ -0,0 +1,1 @@
+export const createLoadMoreButtonTemplate = () => '<button class="load-more" type="button">load more</button>';
