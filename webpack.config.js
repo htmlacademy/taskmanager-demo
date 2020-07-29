66d396f9b563363aa1e5b2a11d45206957634b4e
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:25:19 +0300

    2.5 Настраиваем сервер для разработки
    - Установим пакет сервера webpack-dev-server
    - Опишем скрипт start для запуска сервера локально
    - Настроим сервер в файле настроек webpack
    
    N.B. Теперь, если выполнить в терминале команду npm run start, запустится локальный сервер для разработки. По умолчанию он будет доступен по адресу http://localhost:8080

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+  }
 };
