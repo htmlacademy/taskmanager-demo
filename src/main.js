316bdc31352061eb7425a11dd55a716b4c4b2cfe
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:39:50 +0300

    1.5 Пробуем разбить код на модули
    - Создадим отдельный файл для функции по созданию шаблона меню
    - Импортируем этот файл в main.js и используем функцию createSiteMenuTemplate уже из него

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import {createSiteMenuTemplate} from "./view/site-menu.js";
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
