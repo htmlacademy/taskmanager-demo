e7c6189860f989761a51ff32b8d4a442a32e69b4
Sasha Sushko <sushko@outlook.com>
2020-07-29 18:39:50 +0300

    2.7 Пробуем разбить код на модули
    - Удалим более не нужное указание строго режима
    - Вынесем функцию по созданию шаблона меню в отдельный файл
    - Импортируем этот файл в main.js и используем функцию createSiteMenuTemplate уже из него

--- src/main.js
+++ src/main.js
@@ -1,39 +1,6 @@
-"use strict";
+import {createSiteMenuTemplate} from "./view/site-menu.js";
 
 const TASK_COUNT = 3;
-
-const createSiteMenuTemplate = () => {
-  return (
-    `<section class="control__btn-wrap">
-      <input
-        type="radio"
-        name="control"
-        id="control__new-task"
-        class="control__input visually-hidden"
-      />
-      <label for="control__new-task" class="control__label control__label--new-task"
-        >+ ADD NEW TASK</label
-      >
-      <input
-        type="radio"
-        name="control"
-        id="control__task"
-        class="control__input visually-hidden"
-        checked
-      />
-      <label for="control__task" class="control__label">TASKS</label>
-      <input
-        type="radio"
-        name="control"
-        id="control__statistic"
-        class="control__input visually-hidden"
-      />
-      <label for="control__statistic" class="control__label"
-        >STATISTICS</label
-      >
-    </section>`
-  );
-};
 
 const createFilterTemplate = () => {
   return (
