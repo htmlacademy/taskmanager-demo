4c9b58dae70738c0474bbcaf55489880f223b770
Sasha Sushko <sushko@outlook.com>
2020-07-31 17:15:12 +0300

    2.1 Добавит моковые данные
    - Создадим модуль со вспомогательной функцией по получению случайного элемента массива
    - Создадим модуль с константами
    - Создадим массив с объектами-задачами
    - Объявим функцию по получению случайной задачи

---
+++ src/mock/task.js
@@ -0,0 +1,75 @@
+import {getRandomArrayElement} from '../utils.js';
+import {COLORS} from '../const.js';
+
+const mockTasks = [
+  {
+    description: 'Сделать домашку',
+    dueDate: null,
+    repeating: {
+      mo: false,
+      tu: false,
+      we: true,
+      th: false,
+      fr: false,
+      sa: true,
+      su: false,
+    },
+    color: getRandomArrayElement(COLORS),
+    isArchive: false,
+    isFavorite: true,
+  },
+  {
+    description: 'Завести кота',
+    dueDate: null,
+    repeating: {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false,
+    },
+    color: getRandomArrayElement(COLORS),
+    isArchive: false,
+    isFavorite: false,
+  },
+  {
+    description: 'Купить доллары',
+    dueDate: new Date('2014-01-01'),
+    repeating: {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false,
+    },
+    color: getRandomArrayElement(COLORS),
+    isArchive: true,
+    isFavorite: false,
+  },
+  {
+    description: 'Пройти интенсив на соточку',
+    dueDate: new Date('2023-06-26'),
+    repeating: {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false,
+    },
+    color: getRandomArrayElement(COLORS),
+    isArchive: false,
+    isFavorite: true,
+  }
+];
+
+function getRandomTask() {
+  return getRandomArrayElement(mockTasks);
+}
+
+export {getRandomTask};
