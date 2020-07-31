65745e52c6383619bbc7feb60c9c1fc6ee7d20af
Sasha Sushko <sushko@outlook.com>
2020-07-31 19:32:39 +0300

    2.7 Обучит компонент формы принимать данные. WIP
    N.B. Для отображения формы редактирования и формы создания новой задачи в итоге будет использоваться один компонент, поэтому мы должны подумать о ситуации, когда данные переданы не будут
    
    WIP: линтер ругается на неиспользуемые переменные

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -1,7 +1,7 @@
 import {generateTask} from '../mock/task.js';
 
 export default class TasksModel {
-  tasks = Array.from({length: 3}, generateTask);
+  tasks = Array.from({length: 4}, generateTask);
 
   getTasks = () => this.tasks;
 }
