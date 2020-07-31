bee5a7255a6465c20da6585b61b7cc9dbf80cfe2
Sasha Sushko <sushko@outlook.com>
2020-07-31 17:31:25 +0300

    3.2 Подгоняем моковые данные к реальным
    Для разработки удобнее, если моковые данные близки к реальным, а не являются набором статичных значений. Поэтому добавим в наши моки ещё больше случайности:
    
    - Опишем функцию для генерации даты. По заданию это либо null, либо дата плюс-минус неделя от текущей
    - Для удобства генерации заведем вспомогательную функцию getRandomInteger
    - Сгенерируем дату в итоговом объекте с моками

--- src/mock/task.js
+++ src/mock/task.js
@@ -19,10 +19,39 @@ const generateDescription = () => {
   return descriptions[randomIndex];
 };
 
+const generateDate = () => {
+  // Когда в руках молоток, любая проблема - гвоздь.
+  // Вот и для генерации случайного булевого значения
+  // можно использовать "функцию из интернета".
+  // Ноль - ложь, один - истина. Для верности приводим
+  // к булевому типу с помощью Boolean
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+
+  // По заданию дедлайн у задачи устанавливается без учёта времеми,
+  // но объект даты без времени завести нельзя,
+  // поэтому будем считать срок у всех задач -
+  // это 23:59:59 установленной даты
+  currentDate.setHours(23, 59, 59, 999);
+
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
 export const generateTask = () => {
+  const dueDate = generateDate();
+
   return {
     description: generateDescription(),
-    dueDate: null,
+    dueDate,
     repeating: {
       mo: false,
       tu: false,
