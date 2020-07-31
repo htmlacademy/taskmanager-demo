92425c50b26b7f26698009cf6470bb9cdef37e1d
Sasha Sushko <sushko@outlook.com>
2020-07-31 17:31:25 +0300

    2.2 Подгонит моковые данные к реальным
    Для разработки удобнее, если моковые данные близки к реальным, а не являются набором статичных значений. Поэтому добавим в наши моки ещё больше случайности:
    
    - Опишем функцию для генерации даты. По заданию это либо null, либо дата плюс-минус неделя от текущей
    - Для удобства генерации заведем вспомогательную функцию getRandomInteger
    - Сгенерируем дату в итоговом объекте с моками

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 const getRandomInteger = (a = 0, b = 1) => {
@@ -19,19 +21,41 @@ const generateDescription = () => {
   return descriptions[randomIndex];
 };
 
-export const generateTask = () => ({
-  description: generateDescription(),
-  dueDate: null,
-  repeating: {
-    mo: false,
-    tu: false,
-    we: false,
-    th: false,
-    fr: false,
-    sa: false,
-    su: false,
-  },
-  color: 'black',
-  isArchive: false,
-  isFavorite: false,
-});
+const generateDate = () => {
+  // Когда в руках молоток, любая проблема - гвоздь.
+  // Вот и для генерации случайного булевого значения
+  // можно использовать "функцию из интернета".
+  // Ноль - ложь, один - истина. Для верности приводим
+  // к булевому типу с помощью Boolean
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+
+  return dayjs().add(daysGap, 'day').toDate();
+};
+
+export const generateTask = () => {
+  const dueDate = generateDate();
+
+  return {
+    description: generateDescription(),
+    dueDate,
+    repeating: {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false,
+    },
+    color: 'black',
+    isArchive: false,
+    isFavorite: false,
+  };
+};
