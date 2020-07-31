f546b1ccdbaa64bc8ed0d1610e410b97068deb65
Sasha Sushko <sushko@outlook.com>
2020-07-31 19:18:04 +0300

    2.9 Описываем остальную логику отображения
    По аналогии с датой пишем код, который будет добавлять класс-модификатор повторяющейся задаче, а также выделять кнопки "Архив" и "В избранное" в соответствии с флагами в данных

--- src/view/task.js
+++ src/view/task.js
@@ -4,8 +4,12 @@ const isExpired = (dueDate) => {
   return dueDate === null ? false : dayjs().isAfter(dueDate, `D`);
 };
 
+const isRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
+
 export const createTaskTemplate = (task) => {
-  const {color, description, dueDate} = task;
+  const {color, description, dueDate, repeating, isArchive, isFavorite} = task;
 
   const date = dueDate !== null
     ? dayjs(dueDate).format(`D MMMM`)
@@ -15,19 +19,31 @@ export const createTaskTemplate = (task) => {
     ? `card--deadline`
     : ``;
 
-  return `<article class="card card--${color} ${deadlineClassName}">
+  const repeatClassName = isRepeating(repeating)
+    ? `card--repeat`
+    : ``;
+
+  const archiveClassName = isArchive
+    ? `card__btn--archive card__btn--disabled`
+    : `card__btn--archive`;
+
+  const favoriteClassName = isFavorite
+    ? `card__btn--favorites card__btn--disabled`
+    : `card__btn--favorites`;
+
+  return `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
     <div class="card__form">
       <div class="card__inner">
         <div class="card__control">
           <button type="button" class="card__btn card__btn--edit">
             edit
           </button>
-          <button type="button" class="card__btn card__btn--archive">
+          <button type="button" class="card__btn ${archiveClassName}">
             archive
           </button>
           <button
             type="button"
-            class="card__btn card__btn--favorites"
+            class="card__btn ${favoriteClassName}"
           >
             favorites
           </button>
