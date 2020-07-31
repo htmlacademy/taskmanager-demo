9ba781713eeb541adc0463b1c6488ed975160de6
Sasha Sushko <sushko@outlook.com>
2020-07-31 18:31:30 +0300

    2.4 Обучит компонент задачи принимать данные
    - Добавим в класс TaskView конструктор и сохраним переданные данные
    - Передадим данные о задаче в шаблон
    - Вместо статичных данных в шаблоне выведем моковые данные: цвет карточки, описание, дату
    - Используем библиотеку day.js для вывода дедлайна в человеческом формате
    - По заданию дата может отсутствовать, учтём это

--- src/view/task-view.js
+++ src/view/task-view.js
@@ -1,53 +1,66 @@
 import {createElement} from '../render.js';
+import {humanizeTaskDueDate} from '../utils.js';
 
-const createTaskTemplate = () => (
-  `<article class="card card--black">
-    <div class="card__form">
-      <div class="card__inner">
-        <div class="card__control">
-          <button type="button" class="card__btn card__btn--edit">
-            edit
-          </button>
-          <button type="button" class="card__btn card__btn--archive">
-            archive
-          </button>
-          <button
-            type="button"
-            class="card__btn card__btn--favorites"
-          >
-            favorites
-          </button>
-        </div>
+const createTaskTemplate = (task) => {
+  const {color, description, dueDate} = task;
 
-        <div class="card__color-bar">
-          <svg class="card__color-bar-wave" width="100%" height="10">
-            <use xlink:href="#wave"></use>
-          </svg>
-        </div>
+  const date = dueDate !== null
+    ? humanizeTaskDueDate(dueDate)
+    : '';
 
-        <div class="card__textarea-wrap">
-          <p class="card__text">Example default task with default color.</p>
-        </div>
+  return (
+    `<article class="card card--${color}">
+      <div class="card__form">
+        <div class="card__inner">
+          <div class="card__control">
+            <button type="button" class="card__btn card__btn--edit">
+              edit
+            </button>
+            <button type="button" class="card__btn card__btn--archive">
+              archive
+            </button>
+            <button
+              type="button"
+              class="card__btn card__btn--favorites"
+            >
+              favorites
+            </button>
+          </div>
 
-        <div class="card__settings">
-          <div class="card__details">
-            <div class="card__dates">
-              <div class="card__date-deadline">
-                <p class="card__input-deadline-wrap">
-                  <span class="card__date">23 September</span>
-                </p>
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <p class="card__text">${description}</p>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                <div class="card__date-deadline">
+                  <p class="card__input-deadline-wrap">
+                    <span class="card__date">${date}</span>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </article>`
-);
+    </article>`
+  );
+};
 
 export default class TaskView {
+  constructor(task) {
+    this.task = task;
+  }
+
   getTemplate() {
-    return createTaskTemplate();
+    return createTaskTemplate(this.task);
   }
 
   getElement() {
