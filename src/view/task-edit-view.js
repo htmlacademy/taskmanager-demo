708a7fb834e6783f736e9237ff1bee416fe3a819
Sasha Sushko <sushko@outlook.com>
2020-07-31 20:07:17 +0300

    2.10 Добавит логику отображения выбора цвета
    - Вынесем часть шаблона с выбором цвета в отдельную функцию, чтобы не путаться в коде
    - Опишем условия отображения контрола выбора цвета

--- src/view/task-edit-view.js
+++ src/view/task-edit-view.js
@@ -64,6 +64,22 @@ function createTaskEditRepeatingTemplate(repeating) {
   );
 }
 
+function createTaskEditColorsTemplate(currentColor) {
+  return COLORS.map((color) => `<input
+    type="radio"
+    id="color-${color}"
+    class="card__color-input card__color-input--${color} visually-hidden"
+    name="color"
+    value="${color}"
+    ${currentColor === color ? 'checked' : ''}
+  />
+  <label
+    for="color-${color}"
+    class="card__color card__color--${color}"
+    >${color}</label
+  >`).join('');
+}
+
 function createTaskEditTemplate(data) {
   const {color, description, dueDate, repeating} = data;
 
@@ -74,6 +90,8 @@ function createTaskEditTemplate(data) {
     : '';
 
   const repeatingTemplate = createTaskEditRepeatingTemplate(repeating);
+
+  const colorsTemplate = createTaskEditColorsTemplate(color);
 
   return (
     `<article class="card card--edit card--${color} ${repeatingClassName}">
@@ -107,67 +125,7 @@ function createTaskEditTemplate(data) {
             <div class="card__colors-inner">
               <h3 class="card__colors-title">Color</h3>
               <div class="card__colors-wrap">
-                <input
-                  type="radio"
-                  id="color-black-4"
-                  class="card__color-input card__color-input--black visually-hidden"
-                  name="color"
-                  value="black"
-                />
-                <label
-                  for="color-black-4"
-                  class="card__color card__color--black"
-                  >black</label
-                >
-                <input
-                  type="radio"
-                  id="color-yellow-4"
-                  class="card__color-input card__color-input--yellow visually-hidden"
-                  name="color"
-                  value="yellow"
-                  checked
-                />
-                <label
-                  for="color-yellow-4"
-                  class="card__color card__color--yellow"
-                  >yellow</label
-                >
-                <input
-                  type="radio"
-                  id="color-blue-4"
-                  class="card__color-input card__color-input--blue visually-hidden"
-                  name="color"
-                  value="blue"
-                />
-                <label
-                  for="color-blue-4"
-                  class="card__color card__color--blue"
-                  >blue</label
-                >
-                <input
-                  type="radio"
-                  id="color-green-4"
-                  class="card__color-input card__color-input--green visually-hidden"
-                  name="color"
-                  value="green"
-                />
-                <label
-                  for="color-green-4"
-                  class="card__color card__color--green"
-                  >green</label
-                >
-                <input
-                  type="radio"
-                  id="color-pink-4"
-                  class="card__color-input card__color-input--pink visually-hidden"
-                  name="color"
-                  value="pink"
-                />
-                <label
-                  for="color-pink-4"
-                  class="card__color card__color--pink"
-                  >pink</label
-                >
+                ${colorsTemplate}
               </div>
             </div>
           </div>
