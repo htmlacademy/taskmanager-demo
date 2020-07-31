f1729c9f4eacca331fa9bdc3f6ca2ff1409dcaf5
Sasha Sushko <sushko@outlook.com>
2020-07-31 18:31:30 +0300

    2.7 Обучаем шаблон задачи принимать данные
    Вместо статичных данных в шаблоне выводим моковые данные. Начинаем с простых:
    - Цвет карточки
    - Описание
    - Дата. Используем библиотеку day.js для вывода дедлайна в человеческом формате
    - По заданию дата может отсутствовать, учтём это

--- src/view/task.js
+++ src/view/task.js
@@ -1,5 +1,13 @@
-export const createTaskTemplate = () => {
-  return `<article class="card card--black">
+import dayjs from "dayjs";
+
+export const createTaskTemplate = (task) => {
+  const {color, description, dueDate} = task;
+
+  const date = dueDate !== null
+    ? dayjs(dueDate).format(`D MMMM`)
+    : ``;
+
+  return `<article class="card card--${color}">
     <div class="card__form">
       <div class="card__inner">
         <div class="card__control">
@@ -24,7 +32,7 @@ export const createTaskTemplate = () => {
         </div>
 
         <div class="card__textarea-wrap">
-          <p class="card__text">Example default task with default color.</p>
+          <p class="card__text">${description}</p>
         </div>
 
         <div class="card__settings">
@@ -32,7 +40,7 @@ export const createTaskTemplate = () => {
             <div class="card__dates">
               <div class="card__date-deadline">
                 <p class="card__input-deadline-wrap">
-                  <span class="card__date">23 September</span>
+                  <span class="card__date">${date}</span>
                 </p>
               </div>
             </div>
