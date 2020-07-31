6140f7ecc1c78503d681963fabf51341cd91fe6e
Sasha Sushko <sushko@outlook.com>
2020-07-31 18:31:30 +0300

    2.4 Обучит компонент задачи принимать данные
    - Добавим в класс TaskView конструктор и сохраним переданные данные
    - Передадим данные о задаче в шаблон
    - Вместо статичных данных в шаблоне выведем моковые данные: цвет карточки, описание, дату
    - Используем библиотеку day.js для вывода дедлайна в человеческом формате
    - По заданию дата может отсутствовать, учтём это

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 const getRandomInteger = (a = 0, b = 1) => {
@@ -7,4 +9,6 @@ const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export {getRandomInteger};
+const humanizeTaskDueDate = (dueDate) => dayjs(dueDate).format('D MMMM');
+
+export {getRandomInteger, humanizeTaskDueDate};
