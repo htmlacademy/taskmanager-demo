c6632b6889487859c2545906e247b4e3c5a6722c
Sasha Sushko <sushko@outlook.com>
2020-07-31 19:32:39 +0300

    2.10 Генерируем данные для компонента формы

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
 import {createBoardTemplate} from "./view/board.js";
 import {generateTask} from "./mock/task.js";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 4;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 
@@ -24,9 +24,9 @@ render(siteMainElement, createBoardTemplate(), `beforeend`);
 const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = boardElement.querySelector(`.board__tasks`);
 
-render(taskListElement, createTaskEditTemplate(), `beforeend`);
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
+for (let i = 1; i < TASK_COUNT; i++) {
   render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
