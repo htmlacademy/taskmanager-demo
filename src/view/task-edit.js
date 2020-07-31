23d00074f99b6fc85a88e3dba8e67246e5b5e5e5
Sasha Sushko <sushko@outlook.com>
2020-07-31 19:40:53 +0300

    3.11 Обучаем шаблон формы принимать данные. WIP
    N.B. Для отображения формы редактирования и формы создания новой задачи в итоге будет использоваться один компонент, поэтому мы должны подумать о ситуации, когда данные переданы не будут
    
    WIP: линтер ругается на неиспользуемые переменные

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -1,5 +1,20 @@
-export const createTaskEditTemplate = () => {
-  return `<article class="card card--edit card--yellow card--repeat">
+export const createTaskEditTemplate = (task = {}) => {
+  const {
+    color = `black`,
+    description = ``,
+    dueDate = null,
+    repeating = {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false
+    }
+  } = task;
+
+  return `<article class="card card--edit card--${color} card--repeat">
     <form class="card__form" method="get">
       <div class="card__inner">
         <div class="card__color-bar">
@@ -14,7 +29,7 @@ export const createTaskEditTemplate = () => {
               class="card__text"
               placeholder="Start typing your text here..."
               name="text"
-            >This is example of task edit. You can set date and chose repeating days and color.</textarea>
+            >${description}</textarea>
           </label>
         </div>
 
