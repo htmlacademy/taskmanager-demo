ced11cc8fafb266154a15e17d8ee759a11ea9e6d
Sasha Sushko <sushko@outlook.com>
2020-07-31 17:40:50 +0300

    2.12 Сгенерирует реальную реальность
    - Опишем функцию для генерации дней повторения
    - Дни повторения будем выбирать случайно из двух - среды и пятницы
    
    N.B. Если рандомизировать абсолютно все дни, такие данные не будут похожи на реальные, потому что из семи случайных булевых значений почти каждый раз будет хоть одно истинное, а значит почти все задачи будут повторяющимися

--- src/mock/task.js
+++ src/mock/task.js
@@ -31,21 +31,24 @@ const generateDate = () => {
   return dayjs().add(daysGap, 'day').toDate();
 };
 
+const generateRepeating = () => ({
+  mo: false,
+  tu: false,
+  we: Boolean(getRandomInteger(0, 1)),
+  th: false,
+  fr: Boolean(getRandomInteger(0, 1)),
+  sa: false,
+  su: false,
+});
+
 export const generateTask = () => {
   const dueDate = generateDate();
+  const repeating = generateRepeating();
 
   return {
     description: generateDescription(),
     dueDate,
-    repeating: {
-      mo: false,
-      tu: false,
-      we: false,
-      th: false,
-      fr: false,
-      sa: false,
-      su: false,
-    },
+    repeating,
     color: 'black',
     isArchive: false,
     isFavorite: false,
