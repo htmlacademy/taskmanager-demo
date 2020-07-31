eaf609eed901e26dd5e092ee721e4c18379e1f58
Sasha Sushko <sushko@outlook.com>
2020-07-31 19:18:04 +0300

    2.6 Добавит остальную логику отображения
    По аналогии с датой пишем код, который будет добавлять класс-модификатор повторяющейся задаче, а также выделять кнопки "Архив" и "В избранное" в соответствии с флагами в данных

--- src/view/task-view.js
+++ src/view/task-view.js
@@ -1,8 +1,8 @@
 import {createElement} from '../render.js';
-import {humanizeTaskDueDate, isTaskExpired} from '../utils.js';
+import {humanizeTaskDueDate, isTaskExpired, isTaskRepeating} from '../utils.js';
 
 const createTaskTemplate = (task) => {
-  const {color, description, dueDate} = task;
+  const {color, description, dueDate, repeating, isArchive, isFavorite} = task;
 
   const date = dueDate !== null
     ? humanizeTaskDueDate(dueDate)
@@ -12,20 +12,32 @@ const createTaskTemplate = (task) => {
     ? 'card--deadline'
     : '';
 
+  const repeatClassName = isTaskRepeating(repeating)
+    ? 'card--repeat'
+    : '';
+
+  const archiveClassName = isArchive
+    ? 'card__btn--archive card__btn--disabled'
+    : 'card__btn--archive';
+
+  const favoriteClassName = isFavorite
+    ? 'card__btn--favorites card__btn--disabled'
+    : 'card__btn--favorites';
+
   return (
-    `<article class="card card--${color} ${deadlineClassName}">
+    `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
       <div class="card__form">
         <div class="card__inner">
           <div class="card__control">
             <button type="button" class="card__btn card__btn--edit">
               edit
             </button>
-            <button type="button" class="card__btn card__btn--archive">
+            <button type="button" class="card__btn ${archiveClassName}">
               archive
             </button>
             <button
               type="button"
-              class="card__btn card__btn--favorites"
+              class="card__btn ${favoriteClassName}"
             >
               favorites
             </button>
