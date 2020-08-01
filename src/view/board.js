3fcc85509d9c7188ad263df06e7525aec7471fb6
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:43:34 +0300

    4.1.3 Добавит наследование простым компонентам
    Простые - компоненты, не требующие переопределения конструктора

--- src/view/board.js
+++ src/view/board.js
@@ -1,25 +1,9 @@
-import {createElement} from '../render.js';
+import AbstractView from './abstract.js';
 
 const createBoardTemplate = () => '<section class="board container"></section>';
 
-export default class Board {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Board extends AbstractView {
   getTemplate() {
     return createBoardTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
