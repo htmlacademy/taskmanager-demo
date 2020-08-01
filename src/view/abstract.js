ed0f1318fc2e7d2d71083cfc8241f28f5476bfdd
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:37:35 +0300

    4.1.2 Вынесет общие методы в абстрактный класс
    - Объявим методы getElement и removeElement
    - Объявим свойство _element
    - Метод getTemplate тоже объявим, но в качестве реализации будем бросать исключение, чтобы разработчик не забывал его переопределить

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -1,7 +1,27 @@
+import {createElement} from '../utils.js';
+
 export default class Abstract {
   constructor() {
     if (new.target === Abstract) {
       throw new Error('Can\'t instantiate Abstract, only concrete one.');
     }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error('Abstract method not implemented: getTemplate');
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
