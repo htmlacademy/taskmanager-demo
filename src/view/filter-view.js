bfbbd51575976a346a06faeca656060e0089dd53
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:43:34 +0300

    4.1.3 Добавит наследование компонентам
    - Простые компоненты просто отнаследуем, а в сложных переопределим конструктор
    - Удалим неиспользуемый модуль render.js

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,4 +1,4 @@
-import {createElement} from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 function createFilterTemplate() {
   return (
@@ -64,20 +64,8 @@ function createFilterTemplate() {
   );
 }
 
-export default class FilterView {
-  getTemplate() {
+export default class FilterView extends AbstractView {
+  get template() {
     return createFilterTemplate();
-  }
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-
-    return this.element;
-  }
-
-  removeElement() {
-    this.element = null;
   }
 }
