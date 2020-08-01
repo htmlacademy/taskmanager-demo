8948913180f6dedf3f93d84cfacddd80778a6b3c
Sasha Sushko <sushko@outlook.com>
2020-08-01 21:38:35 +0300

    2.17 Описываем структуру данных фильтров
    - Опишем функцию генерации моковых данных фильтров. Данные фильтров должны основываться на данных по задачам
    - Заведем "мапу" (от англ. map, карта) - объект, где ключи - названия фильтров, а значения - функции-счетчики
    - Опишем утилитарную функцию по определению, есть ли задачи на сегодня
    
    N.B. Фильтры у нас считают задачи правильно, без учета архивированных,
    как требует техзадание, а на доске отображаются все задачи, даже архивированные. Поэтому может казаться, что числа не совпадают. Всё встанет на свои места, когда мы доберемся до реализации отображения задач по фильтрам.

--- src/utils.js
+++ src/utils.js
@@ -13,6 +13,10 @@ export const isTaskExpired = (dueDate) => {
   return dueDate === null ? false : dayjs().isAfter(dueDate, `D`);
 };
 
+export const isTaskExpiringToday = (dueDate) => {
+  return dueDate === null ? false : dayjs(dueDate).isSame(dayjs(), `D`);
+};
+
 export const isTaskRepeating = (repeating) => {
   return Object.values(repeating).some(Boolean);
 };
