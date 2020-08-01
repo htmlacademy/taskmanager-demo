715cbf92a869251c74cd76b1e5e7e0bc24266428
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:43:34 +0300

    4.1.3 Добавит наследование простым компонентам
    Простые - компоненты, не требующие переопределения конструктора

--- src/view/load-more-button.js
+++ src/view/load-more-button.js
@@ -1,25 +1,9 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createLoadMoreButtonTemplate = () => '<button class="load-more" type="button">load more</button>';
 
-export default class LoadMoreButton {
-  constructor() {
-    this._element = null;
-  }
-
+export default class LoadMoreButton extends AbstractView {
   getTemplate() {
     return createLoadMoreButtonTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
