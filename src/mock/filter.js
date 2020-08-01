927400ac5d751a03f8c044c6a20e2967d4b4b581
Sasha Sushko <sushko@outlook.com>
2020-08-01 21:38:35 +0300

    2.17 Добавит структуру данных фильтров
    - Опишем функцию генерации моковых данных фильтров. Данные фильтров должны основываться на данных по задачам
    - Заведем объект, ключами которого будут названия фильтров, а значениями - функции-счетчики
    - Опишем утилитарную функцию по определению, есть ли задачи на сегодня
    
    N.B. Фильтры у нас считают задачи правильно, без учета архивированных,
    как требует техзадание, а на доске отображаются все задачи, даже архивированные. Поэтому может казаться, что числа не совпадают. Всё встанет на свои места, когда мы доберемся до реализации отображения задач по фильтрам.

---
+++ src/mock/filter.js
@@ -0,0 +1,25 @@
+import {isTaskExpired, isTaskRepeating, isTaskExpiringToday} from '../utils.js';
+
+const taskToFilterMap = {
+  all: (tasks) => tasks.filter((task) => !task.isArchive).length,
+  overdue: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isTaskExpired(task.dueDate)).length,
+  today: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isTaskExpiringToday(task.dueDate)).length,
+  favorites: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => task.isFavorite).length,
+  repeating: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isTaskRepeating(task.repeating)).length,
+  archive: (tasks) => tasks.filter((task) => task.isArchive).length,
+};
+
+export const generateFilter = (tasks) => Object.entries(taskToFilterMap).map(
+  ([filterName, countTasks]) => ({
+    name: filterName,
+    count: countTasks(tasks),
+  }),
+);
