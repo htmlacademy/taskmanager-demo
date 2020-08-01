532880bee7ed566a47d573b0dd6ec2bb226d93e1
Sasha Sushko <sushko@outlook.com>
2020-08-01 21:39:28 +0300

    4.2.2 Подключит моки фильтров
    - Импортируем функцию по генерации в точке входа
    - Передадим данные из модели в компонент

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/tasks-model.js';
+import {generateFilter} from './mock/filter.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
@@ -10,7 +11,9 @@ const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const tasksModel = new TasksModel();
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
+const filters = generateFilter(tasksModel.tasks);
+
 render(new NewTaskButtonView(), siteHeaderElement);
-render(new FilterView(), siteMainElement);
+render(new FilterView(filters), siteMainElement);
 
 boardPresenter.init();
