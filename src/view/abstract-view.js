e086e367dbcb5f0a477388e86bf8764c60882b6b
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:37:35 +0300

    4.1.2 Вынесет общие методы в абстрактный класс
    - Объявим геттер element и метод removeElement
    - Объявим свойство #element
    - Геттер template тоже объявим, но в качестве реализации будем бросать исключение, чтобы разработчик не забывал его переопределить

--- src/view/abstract-view.js
+++ src/view/abstract-view.js
@@ -1,7 +1,27 @@
+import {createElement} from '../render.js';
+
 export default class AbstractView {
+  #element = null;
+
   constructor() {
     if (new.target === AbstractView) {
       throw new Error('Can\'t instantiate AbstractView, only concrete one.');
     }
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    throw new Error('Abstract method not implemented: get template');
+  }
+
+  removeElement() {
+    this.#element = null;
   }
 }
