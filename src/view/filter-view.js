5bf0cc93655cfefeae651fe2658e24a46fc3bc72
Sasha Sushko <sushko@outlook.com>
2020-08-01 21:40:30 +0300

    4.2.3 Обучит компонент фильтра принимать данные
    Делаем всё по аналогии с компонентами карточки и формы редактирования задачи

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,71 +1,44 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createFilterTemplate() {
+function createFilterItemTemplate(filter, isChecked) {
+  const {type, count} = filter;
+
+  return (
+    `<input
+      type="radio"
+      id="filter__${type}"
+      class="filter__input visually-hidden"
+      name="filter"
+      ${isChecked ? 'checked' : ''}
+      ${count === 0 ? 'disabled' : ''}
+    />
+    <label for="filter__${type}" class="filter__label">
+      ${type} <span class="filter__${type}-count">${count}</span></label
+    >`
+  );
+}
+
+function createFilterTemplate(filterItems) {
+  const filterItemsTemplate = filterItems
+    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
+    .join('');
+
   return (
     `<section class="main__filter filter container">
-      <input
-        type="radio"
-        id="filter__all"
-        class="filter__input visually-hidden"
-        name="filter"
-        checked
-      />
-      <label for="filter__all" class="filter__label">
-        All <span class="filter__all-count">13</span></label
-      >
-      <input
-        type="radio"
-        id="filter__overdue"
-        class="filter__input visually-hidden"
-        name="filter"
-        disabled
-      />
-      <label for="filter__overdue" class="filter__label"
-        >Overdue <span class="filter__overdue-count">0</span></label
-      >
-      <input
-        type="radio"
-        id="filter__today"
-        class="filter__input visually-hidden"
-        name="filter"
-        disabled
-      />
-      <label for="filter__today" class="filter__label"
-        >Today <span class="filter__today-count">0</span></label
-      >
-      <input
-        type="radio"
-        id="filter__favorites"
-        class="filter__input visually-hidden"
-        name="filter"
-      />
-      <label for="filter__favorites" class="filter__label"
-        >Favorites <span class="filter__favorites-count">1</span></label
-      >
-      <input
-        type="radio"
-        id="filter__repeating"
-        class="filter__input visually-hidden"
-        name="filter"
-      />
-      <label for="filter__repeating" class="filter__label"
-        >Repeating <span class="filter__repeating-count">1</span></label
-      >
-      <input
-        type="radio"
-        id="filter__archive"
-        class="filter__input visually-hidden"
-        name="filter"
-      />
-      <label for="filter__archive" class="filter__label"
-        >Archive <span class="filter__archive-count">115</span></label
-      >
+      ${filterItemsTemplate}
     </section>`
   );
 }
 
 export default class FilterView extends AbstractView {
+  #filters = null;
+
+  constructor({filters}) {
+    super();
+    this.#filters = filters;
+  }
+
   get template() {
-    return createFilterTemplate();
+    return createFilterTemplate(this.#filters);
   }
 }
