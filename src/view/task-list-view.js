6ae0916ee745f2d3b884d14d3ddf4f404196d7d9
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:43:34 +0300

    4.1.3 Добавит наследование простым компонентам
    Простые - компоненты, не требующие конструктора

--- src/view/task-list-view.js
+++ src/view/task-list-view.js
@@ -1,23 +1,9 @@
-import {createElement} from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 const createTaskListTemplate = () => '<div class="board__tasks"></div>';
 
-export default class TaskListView {
-  #element = null;
-
+export default class TaskListView extends AbstractView {
   get template() {
     return createTaskListTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
