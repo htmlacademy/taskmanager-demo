619e918214b3440cc206443f7ebbceec0e37edb3
Sasha Sushko <sushko@outlook.com>
2020-08-02 01:37:35 +0300

    4.1.2 Установит лоадеры
    - Установим лоадеры для Вебпака, чтобы можно было импортировать стили прямо в компонент
    - Подключим лоадеры в настройках Вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
           test: /\.js$/,
           exclude: /(node_modules)/,
           use: ['babel-loader']
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
         }
     ]
   }
