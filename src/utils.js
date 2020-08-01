947de07c125dd83b8b5a4a3e66d6a4cdbac513a7
Sasha Sushko <sushko@outlook.com>
2020-08-01 20:15:24 +0300

    2.15 Добавит рефакторинг
    - Вынесем повторяющиеся данные в модуль с константами
    - Вынесем вспомогательные функции в модуль с утилитами

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
