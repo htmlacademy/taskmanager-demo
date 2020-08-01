418e24f5a7118991d1a9f9277bd4a63818edc6aa
Sasha Sushko <sushko@outlook.com>
2020-08-02 00:26:21 +0300

    3.2.3 Добавит новый компонент - заглушку
    - Создаем новый компонент - заглушку
    - По условию - нет задач - рендерим заглушку на страницу
    - Если задачи есть, то показываем компонент сортировки и карточки
    
    N.B. Чтобы проверить, что заглушка показывается, сгенерируем пустой массив задач

---
+++ src/view/no-task-view.js
@@ -0,0 +1,27 @@
+import {createElement} from '../render.js';
+
+const createNoTaskTemplate = () => (
+  `<p class="board__no-tasks">
+    Click «ADD NEW TASK» in menu to create your first task
+  </p>`
+);
+
+export default class NoTaskView {
+  #element = null;
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    return createNoTaskTemplate();
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
