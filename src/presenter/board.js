fa4952b4b6f0dce0f42ef8e495fb07ff1fffcdb9
Sasha Sushko <sushko@outlook.com>
2020-08-04 10:44:28 +0300

    5.1.7 Описываем презентер доски. WIP
    Презентер в итоге заменит собой функцию renderBoard. Начнём по порядку:
    
    - Объявим конструктор. Он будет получать контейнер, куда рендерить доску, и данные, задачи для рендеринга
    - Перенесем импорты и создание экземпляров дочерних компонентов
    - Опишем, пока текстом, логику работы
    
    WIP: линтер ругается на неиспользуемые переменные

---
+++ src/presenter/board.js
@@ -0,0 +1,52 @@
+import BoardView from "../view/board.js";
+import SortView from "../view/sort.js";
+import TaskListView from "../view/task-list.js";
+import NoTaskView from "../view/no-task.js";
+import TaskView from "../view/task.js";
+import TaskEditView from "../view/task-edit.js";
+import LoadMoreButtonView from "../view/load-more-button.js";
+import {render, RenderPosition} from "../utils/render.js";
+
+export default class Board {
+  constructor(boardContainer) {
+    this._boardContainer = boardContainer;
+
+    this._boardComponent = new BoardView();
+    this._sortComponent = new SortView();
+    this._taskListComponent = new TaskListView();
+    this._noTaskComponent = new NoTaskView();
+  }
+
+  init(boardTasks) {
+    this._boardTasks = boardTasks.slice();
+    // Метод для инициализации (начала работы) модуля,
+    // малая часть текущей функции renderBoard в main.js
+  }
+
+  _renderSort() {
+    // Метод для рендеринга сортировки
+  }
+
+  _renderTask() {
+    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
+    // текущая функция renderTask в main.js
+  }
+
+  _renderTasks() {
+    // Метод для рендеринга N-задач за раз
+  }
+
+  _renderNoTasks() {
+    // Метод для рендеринга заглушки
+  }
+
+  _renderLoadMoreButton() {
+    // Метод, куда уйдёт логика по отрисовке компонетов задачи,
+    // текущая функция renderTask в main.js
+  }
+
+  _renderBoard() {
+    // Метод для инициализации (начала работы) модуля,
+    // бОльшая часть текущей функции renderBoard в main.js
+  }
+}
