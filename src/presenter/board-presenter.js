b41e8f23a4712f0af893ab7b8f687bb37c290461
Sasha Sushko <sushko@outlook.com>
2020-08-05 00:38:04 +0300

    5.2.3 Опишет логику сортировки. WIP
    - Добавляем пустой (пока) обработчик смены сортировки
    - В обработчике помечаем будущую логику
    
    WIP: линтер ругается на неиспользуемые переменные

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -44,8 +44,15 @@ export default class BoardPresenter {
     this.#taskPresenter.get(updatedTask.id).init(updatedTask);
   }
 
+  #handleSortTypeChange = (sortType) => {
+    // - Сортируем задачи
+    // - Очищаем список
+    // - Рендерим список заново
+  }
+
   #renderSort = () => {
     render(this.#boardComponent, this.#sortComponent, RenderPosition.AFTERBEGIN);
+    this.#sortComponent.setSortTypeChangeHandler(this.#handleSortTypeChange);
   }
 
   #renderTask = (task) => {
