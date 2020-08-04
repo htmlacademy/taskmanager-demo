b8831b48c1e2d3865955ce6873c2f0c2f6aab32f
Sasha Sushko <sushko@outlook.com>
2020-08-05 00:29:35 +0300

    5.2.1 Правим шаблон сортировки
    - Добавляем data-атрибуты, чтобы при клике можно было определить, какой тип сортировки выбрал пользователь
    - Все возможные типы сортировки описываем в константе

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,10 +1,11 @@
 import AbstractView from "./abstract.js";
+import {SortType} from "../const.js";
 
 const createSortTemplate = () => {
   return `<div class="board__filter-list">
-    <a href="#" class="board__filter">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter">SORT BY DATE up</a>
-    <a href="#" class="board__filter">SORT BY DATE down</a>
+    <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
+    <a href="#" class="board__filter" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
+    <a href="#" class="board__filter" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
   </div>`;
 };
 
