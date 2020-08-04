189fac82427025c24e035f79b6e17084bed49b00
Sasha Sushko <sushko@outlook.com>
2020-08-04 12:01:54 +0300

    5.1.4 Перенесет в презентер метод renderTask. WIP
    Переносим "как есть", но с одним отличием - теперь taskListComponent можно не передавать, а зачитать из свойства класса презентера

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,7 +5,7 @@ import NoTaskView from '../view/no-task-view.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
-import {render, RenderPosition} from '../utils/render.js';
+import {render, RenderPosition, replace} from '../utils/render.js';
 
 const TASK_COUNT_PER_STEP = 8;
 
@@ -37,8 +37,36 @@ export default class BoardPresenter {
   }
 
   #renderTask = (task) => {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTask в main.js
+    const taskComponent = new TaskView(task);
+    const taskEditComponent = new TaskEditView(task);
+
+    const replaceCardToForm = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+
+    const replaceFormToCard = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    taskComponent.setEditClickHandler(() => {
+      replaceCardToForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    taskEditComponent.setFormSubmitHandler(() => {
+      replaceFormToCard();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(this.#taskListComponent, taskComponent, RenderPosition.BEFOREEND);
   }
 
   #renderTasks = (from, to) => {
