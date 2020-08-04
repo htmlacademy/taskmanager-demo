eaf223a3498dd47fd5a45fec5591b89400303f46
Sasha Sushko <sushko@outlook.com>
2020-08-05 00:59:46 +0300

    5.2.3 Реализует метод сортировки
    - Объявим в презентере два свойства: для бэкапа массива с задачами и для записи выбранной сортировки
    - Заведем функцию сортировки #sortTasks
    - Опишем в обработчике условие-оптимизацию, которое защитит от лишних перерисовок
    - Опишем switch-блок с функциями сортировки
    - Реализуем в утилитах колбэки для метода sort массива
    
    N.B. Хорошо или плохо мутировать данные - давний спор. В целом, скорее, плохо. В нашем же случае, скорее, хорошо. Потому что благодаря ей, нам не нужно думать о методах перерисовки. Для обновления списка задач, нам будет достаточно (почти) существующего метода #renderTaskList

--- src/utils/task.js
+++ src/utils/task.js
@@ -18,4 +18,34 @@ function isTaskExpiringToday(dueDate) {
   return dueDate && dayjs(dueDate).isSame(dayjs(), 'D');
 }
 
-export {humanizeTaskDueDate, isTaskExpired, isTaskRepeating, isTaskExpiringToday};
+// Функция помещает задачи без даты в конце списка,
+// возвращая нужный вес для колбэка sort
+function getWeightForNullDate(dateA, dateB) {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+}
+
+function sortTaskUp(taskA, taskB) {
+  const weight = getWeightForNullDate(taskA.dueDate, taskB.dueDate);
+
+  return weight ?? dayjs(taskA.dueDate).diff(dayjs(taskB.dueDate));
+}
+
+function sortTaskDown(taskA, taskB) {
+  const weight = getWeightForNullDate(taskA.dueDate, taskB.dueDate);
+
+  return weight ?? dayjs(taskB.dueDate).diff(dayjs(taskA.dueDate));
+}
+
+export {humanizeTaskDueDate, isTaskExpired, isTaskRepeating, isTaskExpiringToday, sortTaskUp, sortTaskDown};
