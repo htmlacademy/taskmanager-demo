63c1b892ae1438c50be9d99159c229c83a3cffa9
Sasha Sushko <sushko@outlook.com>
2020-08-09 21:47:24 +0300

    6.1.5 Используем логику обновления задачи
    - Передадим функцию обновления из презентера доски в презентер задачи
    - В презентере задачи опишем обработчики клика по кнопкам "Архив" и "В избранное"
    - В представлении опишем механизм подписки на эти события
    - Передадим из презентера задачи в представление обработчики
    - Сохранение задачи тоже проведём через _changeData, но, пока форма не редактируется, эффекта видно не будет
    
    Теперь, если вы попробуете кликнуть по кнопкам, то у задачи поменяется признак: в архиве или в избранном она, смотря куда кликнуть. Пока что эта информация нигде не сохраняется, поэтому после перезагрузки страницы все данные сгенерируются заново. Это нормально

--- src/presenter/task.js
+++ src/presenter/task.js
@@ -3,13 +3,16 @@ import TaskEditView from "../view/task-edit.js";
 import {render, RenderPosition, replace, remove} from "../utils/render.js";
 
 export default class Task {
-  constructor(taskListContainer) {
+  constructor(taskListContainer, changeData) {
     this._taskListContainer = taskListContainer;
+    this._changeData = changeData;
 
     this._taskComponent = null;
     this._taskEditComponent = null;
 
     this._handleEditClick = this._handleEditClick.bind(this);
+    this._handleFavoriteClick = this._handleFavoriteClick.bind(this);
+    this._handleArchiveClick = this._handleArchiveClick.bind(this);
     this._handleFormSubmit = this._handleFormSubmit.bind(this);
     this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
   }
@@ -24,6 +27,8 @@ export default class Task {
     this._taskEditComponent = new TaskEditView(task);
 
     this._taskComponent.setEditClickHandler(this._handleEditClick);
+    this._taskComponent.setFavoriteClickHandler(this._handleFavoriteClick);
+    this._taskComponent.setArchiveClickHandler(this._handleArchiveClick);
     this._taskEditComponent.setFormSubmitHandler(this._handleFormSubmit);
 
     if (prevTaskComponent === null || prevTaskEditComponent === null) {
@@ -71,7 +76,32 @@ export default class Task {
     this._replaceCardToForm();
   }
 
-  _handleFormSubmit() {
+  _handleFavoriteClick() {
+    this._changeData(
+        Object.assign(
+            {},
+            this._task,
+            {
+              isFavorite: !this._task.isFavorite
+            }
+        )
+    );
+  }
+
+  _handleArchiveClick() {
+    this._changeData(
+        Object.assign(
+            {},
+            this._task,
+            {
+              isArchive: !this._task.isArchive
+            }
+        )
+    );
+  }
+
+  _handleFormSubmit(task) {
+    this._changeData(task);
     this._replaceFormToCard();
   }
 }
--- src/view/task.js
+++ src/view/task.js
@@ -74,6 +74,8 @@ export default class Task extends AbstractView {
     this._task = task;
 
     this._editClickHandler = this._editClickHandler.bind(this);
+    this._favoriteClickHandler = this._favoriteClickHandler.bind(this);
+    this._archiveClickHandler = this._archiveClickHandler.bind(this);
   }
 
   getTemplate() {
@@ -85,8 +87,28 @@ export default class Task extends AbstractView {
     this._callback.editClick();
   }
 
+  _favoriteClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.favoriteClick();
+  }
+
+  _archiveClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.archiveClick();
+  }
+
   setEditClickHandler(callback) {
     this._callback.editClick = callback;
     this.getElement().querySelector(`.card__btn--edit`).addEventListener(`click`, this._editClickHandler);
+  }
+
+  setFavoriteClickHandler(callback) {
+    this._callback.favoriteClick = callback;
+    this.getElement().querySelector(`.card__btn--favorites`).addEventListener(`click`, this._favoriteClickHandler);
+  }
+
+  setArchiveClickHandler(callback) {
+    this._callback.archiveClick = callback;
+    this.getElement().querySelector(`.card__btn--archive`).addEventListener(`click`, this._archiveClickHandler);
   }
 }
