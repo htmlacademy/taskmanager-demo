94593774f2d82d75716ca1f62a4f6c38b0f278ca
Sasha Sushko <sushko@outlook.com>
2020-08-09 19:20:32 +0300

    5.1.11 Реализует очистку списка
    Для дальнейшей работы - сортировки и фильтрации - нам нужно научить наш презентер очищать список задач. Стараемся по максимуму переиспользовать то, что уже есть - метод destroy. Но чтобы вызвать его у каждого Task-презентера, их нужно запомнить все:
    - Заведем свойство _taskPresenter, где Board-презентер будет хранить ссылки на все Task-презентеры. Можно использовать массив или Set, но оптимальнее будет объект или Map - потому что нам нужно часто обращаться к презентерам. Обратиться по ключу проще, чем найти нужный элемент в массиве или в Set.
    - Ключами для презентеров сделаем id задачи. Предварительно научив наши моки эти id генерировать
    - Для очистки списка будем последовательно вызывать destroy всех Task-презентеров, а после затрем пустым объектом и само свойство _taskPresenter - чтобы "убить" ссылки на Task-презентеры и чтобы сборщик мусора их собрал.

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import {nanoid} from 'nanoid';
 import {COLORS} from '../const.js';
 import {getRandomInteger} from '../utils/common.js';
 
@@ -65,6 +66,7 @@ export const generateTask = () => {
     };
 
   return {
+    id: nanoid(),
     description: generateDescription(),
     dueDate,
     repeating,
