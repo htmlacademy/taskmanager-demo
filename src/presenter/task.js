3ed9e80e4e4720f5b3065ad9ec45dec5e29f0890
Sasha Sushko <sushko@outlook.com>
2020-08-09 15:59:13 +0300

    5.1.9 Выделяем логику задачи в отдельный презентер
    - Создадим презентер задачи
    - Перенесем в него логику по созданию компонента задачи из презентера доски. Функции превращаем в методы класса, переменные - в свойства
    
    N.B. Можно было обойтись без метода init, проделав всю работу в конструкторе, но нужно учиться смотреть немножко вперед, где придется перерисовывать компоненты и нужно будет выполнять повторную инициализацию. Если код перенести в конструктор, придется создавать новый экземпляр, а с отдельным методом - просто вызвать.

---
+++ src/presenter/task.js
@@ -0,0 +1,53 @@
+import TaskView from "../view/task.js";
+import TaskEditView from "../view/task-edit.js";
+import {render, RenderPosition, replace} from "../utils/render.js";
+
+export default class Task {
+  constructor(taskListContainer) {
+    this._taskListContainer = taskListContainer;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._handleEditClick = this._handleEditClick.bind(this);
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+    this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
+  }
+
+  init(task) {
+    this._task = task;
+
+    this._taskComponent = new TaskView(task);
+    this._taskEditComponent = new TaskEditView(task);
+
+    this._taskComponent.setEditClickHandler(this._handleEditClick);
+    this._taskEditComponent.setFormSubmitHandler(this._handleFormSubmit);
+
+    render(this._taskListContainer, this._taskComponent, RenderPosition.BEFOREEND);
+  }
+
+  _replaceCardToForm() {
+    replace(this._taskEditComponent, this._taskComponent);
+    document.addEventListener(`keydown`, this._escKeyDownHandler);
+  }
+
+  _replaceFormToCard() {
+    replace(this._taskComponent, this._taskEditComponent);
+    document.removeEventListener(`keydown`, this._escKeyDownHandler);
+  }
+
+  _escKeyDownHandler(evt) {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      evt.preventDefault();
+      this._replaceFormToCard();
+    }
+  }
+
+  _handleEditClick() {
+    this._replaceCardToForm();
+  }
+
+  _handleFormSubmit() {
+    this._replaceFormToCard();
+  }
+}
