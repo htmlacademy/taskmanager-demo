7b74611a61d014e77cf4f90e8fb140dc54bc985c
Sasha Sushko <sushko@outlook.com>
2020-08-09 21:33:24 +0300

    5.1.5 Реализует логику обновления задачи
    - Опишем в презентере доски обработчик изменений в задаче - #handleTaskChange
    - После обновления данных повторно инициализируем Task-презентер уже с новыми данными

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,4 +2,8 @@ function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-export {getRandomArrayElement};
+function updateItem(items, update) {
+  return items.map((item) => item.id === update.id ? update : item);
+}
+
+export {getRandomArrayElement, updateItem};
