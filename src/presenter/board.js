b2dc5e9082a35e954725c43c508c99a7abbb0795
Sasha Sushko <sushko@outlook.com>
2020-08-09 21:47:24 +0300

    5.1.13 Используем логику обновления задачи
    - Передадим функцию обновления из презентера доски в презентер задачи
    - В презентере задачи опишем обработчики клика по кнопкам "Архив" и "В избранное"
    - В представлении опишем механизм подписки на эти события
    - Передадим из презентера задачи в представление обработчики
    - Сохранение задачи тоже проведём через _changeData, но, пока форма не редактируется, эффекта видно не будет
    
    Теперь, если вы попробуете кликнуть по кнопкам, то у задачи поменяется признак: в архиве или в избранном она, смотря куда кликнуть. Пока что эта информация нигде не сохраняется, поэтому после перезагрузки страницы все данные сгенерируются заново. Это нормально

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -44,7 +44,7 @@ export default class Board {
   }
 
   _renderTask(task) {
-    const taskPresenter = new TaskPresenter(this._taskListComponent);
+    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange);
     taskPresenter.init(task);
     this._taskPresenter[task.id] = taskPresenter;
   }
