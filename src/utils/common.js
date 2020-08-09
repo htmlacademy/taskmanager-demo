8015a05c5b2874f763c03f1f974f246b971c146f
Sasha Sushko <sushko@outlook.com>
2020-08-09 21:33:24 +0300

    5.1.5 Реализует логику обновления задачи
    - Опишем в презентере доски обработчик изменений в задаче - #handleTaskChange
    - После обновления данных повторно инициализируем Task-презентер уже с новыми данными

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,4 +7,18 @@ const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export {getRandomInteger};
+const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
+export {getRandomInteger, updateItem};
