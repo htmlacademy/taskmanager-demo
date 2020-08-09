d6c7fd32b2ccc9d3fbbc04819728a8de6337a157
Sasha Sushko <sushko@outlook.com>
2020-08-09 17:38:54 +0300

    5.1.10 Добавит возможность переиспользования
    - Добавим возможность повторно инициализировать презентер задачи. Для этого в методе init будем запоминать предыдущие компоненты. Если они null, то есть не создавались, рендерим как раньше. Если они отличны от null, то есть создавались, то заменяем их новыми и удаляем
    - Добавим метод destroy для удаления компонентов. Он пригодится нам для очистки списка задач при фильтрации и сортировке

--- src/presenter/task-presenter.js
+++ src/presenter/task-presenter.js
@@ -1,6 +1,6 @@
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
-import {render, RenderPosition, replace} from '../utils/render.js';
+import {render, RenderPosition, replace, remove} from '../utils/render.js';
 
 export default class TaskPresenter {
   #taskListContainer = null;
@@ -17,13 +17,37 @@ export default class TaskPresenter {
   init = (task) => {
     this.#task = task;
 
+    const prevTaskComponent = this.#taskComponent;
+    const prevTaskEditComponent = this.#taskEditComponent;
+
     this.#taskComponent = new TaskView(task);
     this.#taskEditComponent = new TaskEditView(task);
 
     this.#taskComponent.setEditClickHandler(this.#handleEditClick);
     this.#taskEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
 
-    render(this.#taskListContainer, this.#taskComponent, RenderPosition.BEFOREEND);
+    if (prevTaskComponent === null || prevTaskEditComponent === null) {
+      render(this.#taskListContainer, this.#taskComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    // Проверка на наличие в DOM необходима,
+    // чтобы не пытаться заменить то, что не было отрисовано
+    if (this.#taskListContainer.element.contains(prevTaskComponent.element)) {
+      replace(this.#taskComponent, prevTaskComponent);
+    }
+
+    if (this.#taskListContainer.element.contains(prevTaskEditComponent.element)) {
+      replace(this.#taskEditComponent, prevTaskEditComponent);
+    }
+
+    remove(prevTaskComponent);
+    remove(prevTaskEditComponent);
+  }
+
+  destroy = () => {
+    remove(this.#taskComponent);
+    remove(this.#taskEditComponent);
   }
 
   #replaceCardToForm = () => {
