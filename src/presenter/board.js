cc8ad24e0a41a4c7fd30e19f9a319d8a55dc505d
Sasha Sushko <sushko@outlook.com>
2020-08-09 21:33:24 +0300

    6.1.4 Реализуем логику обновления задачи
    - Опишем в презентере доски обработчик изменений в задаче - _handleTaskChange
    - Задачи хранятся в двух списках (из-за сортировки), поэтому нужно обновить данные в обоих. Для этого заведем универсальную функцию обновления элемента массива updateItem
    - После обновления данных повторно инициализируем Task-презентер уже с новыми данными

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -4,6 +4,7 @@ import TaskListView from "../view/task-list.js";
 import NoTaskView from "../view/no-task.js";
 import LoadMoreButtonView from "../view/load-more-button.js";
 import TaskPresenter from "./task.js";
+import {updateItem} from "../utils/common.js";
 import {render, RenderPosition, remove} from "../utils/render.js";
 import {sortTaskUp, sortTaskDown} from "../utils/task.js";
 import {SortType} from "../const.js";
@@ -23,6 +24,7 @@ export default class Board {
     this._noTaskComponent = new NoTaskView();
     this._loadMoreButtonComponent = new LoadMoreButtonView();
 
+    this._handleTaskChange = this._handleTaskChange.bind(this);
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
     this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
   }
@@ -38,6 +40,12 @@ export default class Board {
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
 
     this._renderBoard();
+  }
+
+  _handleTaskChange(updatedTask) {
+    this._boardTasks = updateItem(this._boardTasks, updatedTask);
+    this._sourcedBoardTasks = updateItem(this._sourcedBoardTasks, updatedTask);
+    this._taskPresenter[updatedTask.id].init(updatedTask);
   }
 
   _sortTasks(sortType) {
