3b5b6216a05bda9a9c73ca1945073ab49ebdb2a5
Sasha Sushko <sushko@outlook.com>
2020-08-09 15:59:13 +0300

    5.1.2 Выделит логику задачи в отдельный презентер
    - Создадим презентер задачи
    - Перенесем в него логику по созданию компонента задачи из презентера доски. Функции превращаем в методы класса, переменные - в свойства
    
    N.B. Можно было обойтись без метода init, проделав всю работу в конструкторе, но нужно учиться смотреть немножко вперед, где придется перерисовывать компоненты и нужно будет выполнять повторную инициализацию. Если код перенести в конструктор, придется создавать новый экземпляр, а с отдельным методом - просто вызвать.

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,11 +1,10 @@
-import {render, RenderPosition, replace, remove} from '../framework/render.js';
+import {render, RenderPosition, remove} from '../framework/render.js';
 import BoardView from '../view/board-view.js';
 import SortView from '../view/sort-view.js';
 import TaskListView from '../view/task-list-view.js';
-import TaskView from '../view/task-view.js';
-import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 import NoTaskView from '../view/no-task-view.js';
+import TaskPresenter from './task-presenter.js';
 
 const TASK_COUNT_PER_STEP = 8;
 
@@ -46,37 +45,10 @@ export default class BoardPresenter {
   }
 
   #renderTask(task) {
-    const escKeyDownHandler = (evt) => {
-      if (evt.key === 'Escape') {
-        evt.preventDefault();
-        replaceFormToCard();
-        document.removeEventListener('keydown', escKeyDownHandler);
-      }
-    };
-    const taskComponent = new TaskView({
-      task,
-      onEditClick: () => {
-        replaceCardToForm();
-        document.addEventListener('keydown', escKeyDownHandler);
-      }
+    const taskPresenter = new TaskPresenter({
+      taskListContainer: this.#taskListComponent.element,
     });
-    const taskEditComponent = new TaskEditView({
-      task,
-      onFormSubmit: () => {
-        replaceFormToCard();
-        document.removeEventListener('keydown', escKeyDownHandler);
-      }
-    });
-
-    function replaceCardToForm() {
-      replace(taskEditComponent, taskComponent);
-    }
-
-    function replaceFormToCard() {
-      replace(taskComponent, taskEditComponent);
-    }
-
-    render(taskComponent, this.#taskListComponent.element);
+    taskPresenter.init(task);
   }
 
   #renderTasks(from, to) {
