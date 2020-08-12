fc77ef096cdd4b59b03b63486411fd94ce0e5f0b
Sasha Sushko <sushko@outlook.com>
2020-08-13 01:41:38 +0300

    5.1.7 Ограничит количество форм за раз
    Схема следующая: при попытке открыть задачу на редактирование мы просим все презентеры задач сменить режим с редактирования на просмотр.
    
    - В презентере задачи заведём поле-флаг - в каком режиме задача: просмотр (по умолчанию) или редактирование
    - Реализуем метод resetView для смены режима на тот, что по умолчанию
    - В презентере доски опишем метод уведомления всех презентеров о смене режима
    - В resetView опишем условие, чтобы режим сменялся только если задача была открыта на редактирование
    - На этот же режим завяжем проверку, какой компонент заменять при повторной инициализации

--- src/presenter/task-presenter.js
+++ src/presenter/task-presenter.js
@@ -2,18 +2,26 @@ import {render, replace, remove} from '../framework/render.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
+
 export default class TaskPresenter {
   #taskListContainer = null;
   #changeData = null;
+  #changeMode = null;
 
   #taskComponent = null;
   #taskEditComponent = null;
 
   #task = null;
+  #mode = Mode.DEFAULT;
 
-  constructor(taskListContainer, changeData) {
+  constructor(taskListContainer, changeData, changeMode) {
     this.#taskListContainer = taskListContainer;
     this.#changeData = changeData;
+    this.#changeMode = changeMode;
   }
 
   init = (task) => {
@@ -35,13 +43,11 @@ export default class TaskPresenter {
       return;
     }
 
-    // Проверка на наличие в DOM необходима,
-    // чтобы не пытаться заменить то, что не было отрисовано
-    if (this.#taskListContainer.contains(prevTaskComponent.element)) {
+    if (this.#mode === Mode.DEFAULT) {
       replace(this.#taskComponent, prevTaskComponent);
     }
 
-    if (this.#taskListContainer.contains(prevTaskEditComponent.element)) {
+    if (this.#mode === Mode.EDITING) {
       replace(this.#taskEditComponent, prevTaskEditComponent);
     }
 
@@ -54,14 +60,23 @@ export default class TaskPresenter {
     remove(this.#taskEditComponent);
   };
 
+  resetView = () => {
+    if (this.#mode !== Mode.DEFAULT) {
+      this.#replaceFormToCard();
+    }
+  };
+
   #replaceCardToForm = () => {
     replace(this.#taskEditComponent, this.#taskComponent);
     document.addEventListener('keydown', this.#escKeyDownHandler);
+    this.#changeMode();
+    this.#mode = Mode.EDITING;
   };
 
   #replaceFormToCard = () => {
     replace(this.#taskComponent, this.#taskEditComponent);
     document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#mode = Mode.DEFAULT;
   };
 
   #escKeyDownHandler = (evt) => {
