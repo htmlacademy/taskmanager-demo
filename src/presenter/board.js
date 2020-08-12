d75bc4dfe342573313002dab963e160c4f7b0444
Sasha Sushko <sushko@outlook.com>
2020-08-13 01:41:38 +0300

    5.1.14 Ограничит количество форм за раз
    Схема следующая: при попытке открыть задачу на редактирование мы просим все презентеры задач сменить режим с редактирования на просмотр.
    
    - В презентере задачи заведём поле-флаг - в каком режиме задача: просмотр (по умолчанию) или редактирование
    - Реализуем метод resetView для смены режима на тот, что по умолчанию
    - В презентере доски опишем метод уведомления всех презентеров о смене режима
    - В resetView опишем условие, чтобы режим сменялся только если задача была открыта на редактирование
    - На этот же режим завяжем проверку, какой компонент заменять при повторной инициализации

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -22,6 +22,7 @@ export default class Board {
     this._loadMoreButtonComponent = new LoadMoreButtonView();
 
     this._handleTaskChange = this._handleTaskChange.bind(this);
+    this._handleModeChange = this._handleModeChange.bind(this);
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
   }
 
@@ -34,6 +35,10 @@ export default class Board {
     this._renderBoard();
   }
 
+  _handleModeChange() {
+    this._taskPresenter.forEach((presenter) => presenter.resetView());
+  }
+
   _handleTaskChange(updatedTask) {
     this._boardTasks = updateItem(this._boardTasks, updatedTask);
     this._taskPresenter.get(updatedTask.id).init(updatedTask);
@@ -44,7 +49,7 @@ export default class Board {
   }
 
   _renderTask(task) {
-    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange);
+    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange, this._handleModeChange);
     taskPresenter.init(task);
     this._taskPresenter.set(task.id, taskPresenter);
   }
