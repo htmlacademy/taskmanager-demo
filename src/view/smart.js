638a2c36337081b2a3f2c0f1974fc9761ee5e656
Sasha Sushko <sushko@outlook.com>
2020-08-13 00:47:09 +0300

    6.1.8 Вынесет общую логику обновления
    В будущем нам потребуется обновлять и другие компоненты (например, статистику или фильтры) и хорошо бы переиспользовать подход, который мы уже реализовали, для этого выделим общие методы в отдельный класс Smart

---
+++ src/view/smart.js
@@ -0,0 +1,42 @@
+import AbstractView from './abstract.js';
+
+export default class Smart extends AbstractView {
+  constructor() {
+    super();
+    this._data = {};
+  }
+
+  updateData(update, justDataUpdating) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign(
+      {},
+      this._data,
+      update,
+    );
+
+    if (justDataUpdating) {
+      return;
+    }
+
+    this.updateElement();
+  }
+
+  updateElement() {
+    const prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, prevElement);
+
+    this.restoreHandlers();
+  }
+
+  restoreHandlers() {
+    throw new Error('Abstract method not implemented: restoreHandlers');
+  }
+}
