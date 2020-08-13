bece92b01e42b6b982f99513c2875344934e081d
Sasha Sushko <sushko@outlook.com>
2020-08-14 02:14:11 +0300

    6.2.1 Установит flatpickr. WIP
    - Установим сам пакет flatpickr
    - Установим лоадеры для Вебпака, чтобы можно было импортировать стили прямо в компонент
    - Подключим лоадеры в настройках Вебпака
    - Импортируем в компоненте flatpickr и его стили
    
    WIP: линтер ругается на неиспользуемые переменные

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,13 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     watchContentBase: true,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+        }
+    ]
   }
 };
