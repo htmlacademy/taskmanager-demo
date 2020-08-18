7efd6dc02eb7124b98f944ab75f7ed2e2b86b413
Sasha Sushko <sushko@outlook.com>
2020-08-18 17:26:40 +0300

    7.1.1 Добавит модель в приложение
    - Создадим отдельный класс для модели
    - Создадим отдельный класс для реализации паттерна Наблюдатель. Унаследуем от него модель
    - Объявим методы записи и получения всех задач
    - Создадим экземпляр модели в точке входа
    - Передадим модель в презентер
    - Используем модель в презенторе доски: добавим обертку над методом модели для получения задач, в будущем так будет удобнее получать из модели данные в презенторе
    
    N.B. Пока что данные по-прежнему берутся напрямую из моков

---
+++ src/utils/abstract-observer.js
@@ -0,0 +1,17 @@
+export default class AbstractObserver {
+  constructor() {
+    this._observers = new Set();
+  }
+
+  addObserver(observer) {
+    this._observers.add(observer);
+  }
+
+  removeObserver(observer) {
+    this._observers.delete(observer);
+  }
+
+  _notify(event, payload) {
+    this._observers.forEach((observer) => observer(event, payload));
+  }
+}
