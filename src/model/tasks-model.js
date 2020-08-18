47fa60ce0a8d7630e292a5e65cafa93ffc192786
Sasha Sushko <sushko@outlook.com>
2020-08-18 17:26:40 +0300

    7.1 Использует паттерн Наблюдатель
    - Унаследуем модель от класса Observable, который реализует паттерн Наблюдатель
    - Добавим в презенторе доски обертку над методом модели для получения задач, в будущем так будет удобнее получать из модели данные в презенторе
    
    N.B. Пока что данные по-прежнему берутся напрямую из моков

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -1,6 +1,7 @@
+import Observable from '../framework/observable.js';
 import {generateTask} from '../mock/task.js';
 
-export default class TasksModel {
+export default class TasksModel extends Observable {
   #tasks = Array.from({length: 22}, generateTask);
 
   get tasks() {
