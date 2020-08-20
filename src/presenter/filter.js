0503ea85b1b99b0b71d7aad62825cc78396c3dcf
Sasha Sushko <sushko@outlook.com>
2020-08-21 00:27:14 +0300

    7.1.9 Добавляем презентер фильтров
    - По образу презентера доски создадим презентер фильтров
    - В презентер фильтров передадим модель фильтров и подпишем презентер на ее изменения
    - Научим презентер считать количество задач и формировать данные для представления (метод _getFilters)
    - Используем презентер в точке входа
    
    N.B. Благодаря тому, что "рельсы датабиндинга" в приложении уже настроены, мы с легкостью передаем данные от представления к модели через презентер и обратно

---
+++ src/presenter/filter.js
@@ -0,0 +1,88 @@
+import FilterView from "../view/filter.js";
+import {render, RenderPosition, replace, remove} from "../utils/render.js";
+import {filter} from "../utils/filter.js";
+import {FilterType, UpdateType} from "../const.js";
+
+export default class Filter {
+  constructor(filterContainer, filterModel, tasksModel) {
+    this._filterContainer = filterContainer;
+    this._filterModel = filterModel;
+    this._tasksModel = tasksModel;
+    this._currentFilter = null;
+
+    this._filterComponent = null;
+
+    this._handleModelEvent = this._handleModelEvent.bind(this);
+    this._handleFilterTypeChange = this._handleFilterTypeChange.bind(this);
+
+    this._tasksModel.addObserver(this._handleModelEvent);
+    this._filterModel.addObserver(this._handleModelEvent);
+  }
+
+  init() {
+    this._currentFilter = this._filterModel.getFilter();
+
+    const filters = this._getFilters();
+    const prevFilterComponent = this._filterComponent;
+
+    this._filterComponent = new FilterView(filters, this._currentFilter);
+    this._filterComponent.setFilterTypeChangeHandler(this._handleFilterTypeChange);
+
+    if (prevFilterComponent === null) {
+      render(this._filterContainer, this._filterComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    replace(this._filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
+  }
+
+  _handleModelEvent() {
+    this.init();
+  }
+
+  _handleFilterTypeChange(filterType) {
+    if (this._currentFilter === filterType) {
+      return;
+    }
+
+    this._filterModel.setFilter(UpdateType.MAJOR, filterType);
+  }
+
+  _getFilters() {
+    const tasks = this._tasksModel.getTasks();
+
+    return [
+      {
+        type: FilterType.ALL,
+        name: `All`,
+        count: filter[FilterType.ALL](tasks).length
+      },
+      {
+        type: FilterType.OVERDUE,
+        name: `Overdue`,
+        count: filter[FilterType.OVERDUE](tasks).length
+      },
+      {
+        type: FilterType.TODAY,
+        name: `Today`,
+        count: filter[FilterType.TODAY](tasks).length
+      },
+      {
+        type: FilterType.FAVORITES,
+        name: `Favorites`,
+        count: filter[FilterType.FAVORITES](tasks).length
+      },
+      {
+        type: FilterType.REPEATING,
+        name: `Repeating`,
+        count: filter[FilterType.REPEATING](tasks).length
+      },
+      {
+        type: FilterType.ARCHIVE,
+        name: `Archive`,
+        count: filter[FilterType.ARCHIVE](tasks).length
+      }
+    ];
+  }
+}
