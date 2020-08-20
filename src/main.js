f905ac4e7860c08c762ad604c94ea262251eebc4
Sasha Sushko <sushko@outlook.com>
2020-08-21 00:17:24 +0300

    7.7 Добавит модель для фильтра. WIP
    Задача модели - хранить выбранный фильтр и уведомлять наблюдателей, если таковой изменится.
    
    WIP: линтер ругается на неиспользуемые переменные

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/tasks-model.js';
+import FilterModel from './model/filter-model.js';
 import {generateFilter} from './mock/filter.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter({
   boardContainer: siteMainElement,
   tasksModel,
