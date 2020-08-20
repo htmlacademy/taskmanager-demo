98f9540398c5fd29d727411b2360e5cf12cce8d8
Sasha Sushko <sushko@outlook.com>
2020-08-21 00:25:04 +0300

    7.8 Отрефакторит фильтрацию. WIP
    - Удалим моки для фильтров
    - Шаблон фильтров научим учитывать выбранный фильтр
    - В представлении фильтров реализуем механизм подписки на выбор нового фильтра
    
    WIP: временно отображаются ненастоящие фильтры

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/tasks-model.js';
 import FilterModel from './model/filter-model.js';
-import {generateFilter} from './mock/filter.js';
+
+const filters = [
+  {
+    type: 'all',
+    count: 0,
+  },
+];
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
@@ -15,9 +21,11 @@ const boardPresenter = new BoardPresenter({
   tasksModel,
 });
 
-const filters = generateFilter(tasksModel.tasks);
-
 render(new NewTaskButtonView(), siteHeaderElement);
-render(new FilterView({filters}), siteMainElement);
+render(new FilterView({
+  filters,
+  currentFilterType: 'all',
+  onFilterTypeChange: () => {}
+}), siteMainElement);
 
 boardPresenter.init();
