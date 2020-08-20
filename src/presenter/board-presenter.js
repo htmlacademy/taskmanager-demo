f36151a84f56eb24ce57a14608f58871e1a6e492
Sasha Sushko <sushko@outlook.com>
2020-08-21 02:31:34 +0300

    7.1.12 Реализует черновое добавление задачи
    - Создадим презентер формы добавления
    - Опишем в нем логику появления и скрытия. Она будет похожа на ту, что используется в презентере задачи
    - В презентере доски опишем метод createTask, который будет инициализировать презентер формы добавления
    - Тут же, в презентере доски, добавим вызов метода destroy презентера формы добавления в случаях обновления доски или открытии формы редактирования

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,6 +4,7 @@ import TaskListView from '../view/task-list-view.js';
 import NoTaskView from '../view/no-task-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 import TaskPresenter from './task-presenter.js';
+import TaskNewPresenter from './task-new-presenter.js';
 import {render, RenderPosition, remove} from '../utils/render.js';
 import {sortTaskUp, sortTaskDown} from '../utils/task.js';
 import {filter} from '../utils/filter.js';
@@ -24,6 +25,7 @@ export default class BoardPresenter {
 
   #renderedTaskCount = TASK_COUNT_PER_STEP;
   #taskPresenter = new Map();
+  #taskNewPresenter = null;
   #currentSortType = SortType.DEFAULT;
   #filterType = FilterType.ALL;
 
@@ -31,6 +33,8 @@ export default class BoardPresenter {
     this.#boardContainer = boardContainer;
     this.#tasksModel = tasksModel;
     this.#filterModel = filterModel;
+
+    this.#taskNewPresenter = new TaskNewPresenter(this.#taskListComponent, this.#handleViewAction);
 
     this.#tasksModel.addObserver(this.#handleModelEvent);
     this.#filterModel.addObserver(this.#handleModelEvent);
@@ -58,7 +62,14 @@ export default class BoardPresenter {
     this.#renderBoard();
   }
 
+  createTask = () => {
+    this.#currentSortType = SortType.DEFAULT;
+    this.#filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
+    this.#taskNewPresenter.init();
+  }
+
   #handleModeChange = () => {
+    this.#taskNewPresenter.destroy();
     this.#taskPresenter.forEach((presenter) => presenter.resetView());
   }
 
@@ -147,6 +158,7 @@ export default class BoardPresenter {
   #clearBoard = ({resetRenderedTaskCount = false, resetSortType = false} = {}) => {
     const taskCount = this.tasks.length;
 
+    this.#taskNewPresenter.destroy();
     this.#taskPresenter.forEach((presenter) => presenter.destroy());
     this.#taskPresenter.clear();
 
