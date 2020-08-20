92730d46a74e08782cd0ba0e6424e6868b5334e9
Sasha Sushko <sushko@outlook.com>
2020-08-21 00:25:04 +0300

    7.1.8 Отрефакторит фильтрацию. WIP
    - Удалим моки для фильтров
    - Функции фильтрации перенесем в утилитарный модуль
    - Шаблон фильтров научим учитывать выбранный фильтр
    - В представлении фильтров реализуем механизм подписки на выбор нового фильтра
    
    WIP: временно отображаются ненастоящие фильтры

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import SiteMenuView from './view/site-menu.js';
 import FilterView from './view/filter.js';
 import {render, RenderPosition} from './utils/render.js';
 import {generateTask} from './mock/task.js';
-import {generateFilter} from './mock/filter.js';
 import BoardPresenter from './presenter/board.js';
 import TasksModel from './model/tasks.js';
 import FilterModel from './model/filter.js';
@@ -10,7 +9,13 @@ import FilterModel from './model/filter.js';
 const TASK_COUNT = 22;
 
 const tasks = Array.from({length: TASK_COUNT}, generateTask);
-const filters = generateFilter(tasks);
+const filters = [
+  {
+    type: 'all',
+    name: 'ALL',
+    count: 0,
+  },
+];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -23,6 +28,6 @@ const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterView(filters, 'all'), RenderPosition.BEFOREEND);
 
 boardPresenter.init();
