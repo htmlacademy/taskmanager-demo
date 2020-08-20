b506998ac122f16250af8068b6cc56bfec920ff1
Sasha Sushko <sushko@outlook.com>
2020-08-20 23:36:35 +0300

    7.1.5 Доработает рендеринг доски
    - Опишем новый метод #clearBoard для очистки доски: он умеет очищать список и (по необходимости) сбрасывать количество показанных задач или сортировку
    - #renderBoard объединим с #renderTaskList и тоже научим отрисовывать не просто TASK_COUNT_PER_STEP задач, а столько - сколько было до перерисовки
    - методы отрисовки сортировки и кнопки допоказа рефакторим, чтобы они умели перерисовываться вместе с доской
    - Шаблон сортировки научим учитывать выбранную и отмечать активный пункт. Теперь это возможно, потому что компонент сортировки перерисовывается с доской

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,17 +1,24 @@
 import AbstractView from './abstract-view.js';
 import {SortType} from '../const.js';
 
-const createSortTemplate = () => (
+const createSortTemplate = (currentSortType) => (
   `<div class="board__sort-list">
-    <a href="#" class="board__sort-item" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
-    <a href="#" class="board__sort-item" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
-    <a href="#" class="board__sort-item" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
+    <a href="#" class="board__sort-item ${currentSortType === SortType.DEFAULT ? 'board__sort-item--active' : ''}" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
+    <a href="#" class="board__sort-item ${currentSortType === SortType.DATE_UP ? 'board__sort-item--active' : ''}" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
+    <a href="#" class="board__sort-item ${currentSortType === SortType.DATE_DOWN ? 'board__sort-item--active' : ''}" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
   </div>`
 );
 
 export default class SortView extends AbstractView {
+  #currentSortType = null;
+
+  constructor(currentSortType) {
+    super();
+    this.#currentSortType = currentSortType;
+  }
+
   get template() {
-    return createSortTemplate();
+    return createSortTemplate(this.#currentSortType);
   }
 
   setSortTypeChangeHandler = (callback) => {
