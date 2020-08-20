3d6bb7be123b6e701c37e6fa9ee560a20b86f88f
Sasha Sushko <sushko@outlook.com>
2020-08-20 22:41:46 +0300

    7.1.3 Доработает модель и датабиндинг. WIP
    - Опишем в модели методы добавления, обновления и удаления задачи
    - _handleTaskChange заменим на обработчик любого пользовательского действия (пока пустой)
    - добавим обработчик-наблюдатель _handleModelEvent, который будет реагировать на изменения модели
    
    WIP: линтер ругается на неиспользуемые переменные и консоль. По-прежнему не работает обновление

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -24,10 +24,13 @@ export default class Board {
     this._noTaskComponent = new NoTaskView();
     this._loadMoreButtonComponent = new LoadMoreButtonView();
 
-    this._handleTaskChange = this._handleTaskChange.bind(this);
+    this._handleViewAction = this._handleViewAction.bind(this);
+    this._handleModelEvent = this._handleModelEvent.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
     this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
+
+    this._tasksModel.addObserver(this._handleModelEvent);
   }
 
   init() {
@@ -52,9 +55,20 @@ export default class Board {
     this._taskPresenter.forEach((presenter) => presenter.resetView());
   }
 
-  _handleTaskChange(updatedTask) {
-    // Здесь будем вызывать обновление модели
-    this._taskPresenter.get(updatedTask.id).init(updatedTask);
+  _handleViewAction(actionType, updateType, update) {
+    console.log(actionType, updateType, update);
+    // Здесь будем вызывать обновление модели.
+    // actionType - действие пользователя, нужно чтобы понять, какой метод модели вызвать
+    // updateType - тип изменений, нужно чтобы понять, что после нужно обновить
+    // update - обновленные данные
+  }
+
+  _handleModelEvent(updateType, data) {
+    console.log(updateType, data);
+    // В зависимости от типа изменений решаем, что делать:
+    // - обновить часть списка (например, когда поменялось описание)
+    // - обновить список (например, когда задача ушла в архив)
+    // - обновить всю доску (например, при переключении фильтра)
   }
 
   _handleSortTypeChange(sortType) {
@@ -73,7 +87,7 @@ export default class Board {
   }
 
   _renderTask(task) {
-    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange, this._handleModeChange);
+    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleViewAction, this._handleModeChange);
     taskPresenter.init(task);
     this._taskPresenter.set(task.id, taskPresenter);
   }
