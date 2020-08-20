1db533305e8613465d1725d5220adca806ef77ba
Sasha Sushko <sushko@outlook.com>
2020-08-21 00:27:14 +0300

    7.1.9 Добавит презентер фильтров
    - По образу презентера доски создадим презентер фильтров
    - В презентер фильтров передадим модель фильтров и подпишем презентер на ее изменения
    - Научим презентер считать количество задач и формировать данные для представления (геттер filters)
    - Используем презентер в точке входа
    
    N.B. Благодаря тому, что "рельсы датабиндинга" в приложении уже настроены, мы с легкостью передаем данные от представления к модели через презентер и обратно

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import {render} from './framework/render.js';
 import NewTaskButtonView from './view/new-task-button-view.js';
-import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import TasksModel from './model/tasks-model.js';
 import FilterModel from './model/filter-model.js';
-
-const filters = [
-  {
-    type: 'all',
-    name: 'ALL',
-    count: 0,
-  },
-];
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
@@ -19,8 +11,9 @@ const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const tasksModel = new TasksModel();
 const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
+const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
 
 render(new NewTaskButtonView(), siteHeaderElement);
-render(new FilterView(filters, 'all'), siteMainElement);
 
+filterPresenter.init();
 boardPresenter.init();
