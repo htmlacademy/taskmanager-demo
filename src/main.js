96b2d2cb5f3e6fb5b0d078baf5651e10a12551ca
Sasha Sushko <sushko@outlook.com>
2020-08-21 00:25:04 +0300

    7.1.8 Рефакторинг фильтрации. WIP
    - Удалим моки для фильтров
    - Функции фильтрации перенесем в утилитарный модуль
    - Шаблон фильтров научим учитывать выбранный фильтр
    - В представлении фильтров реализуем механизм подписки на выбор нового фильтра
    
    WIP: временно отображаются ненастоящие фильтры

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import SiteMenuView from "./view/site-menu.js";
 import FilterView from "./view/filter.js";
 import {generateTask} from "./mock/task.js";
-import {generateFilter} from "./mock/filter.js";
 import BoardPresenter from "./presenter/board.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
@@ -10,7 +9,13 @@ import {render, RenderPosition} from "./utils/render.js";
 const TASK_COUNT = 22;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const filters = generateFilter(tasks);
+const filters = [
+  {
+    type: `all`,
+    name: `ALL`,
+    count: 0
+  }
+];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -23,6 +28,6 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterView(filters, `all`), RenderPosition.BEFOREEND);
 
 boardPresenter.init();
--- src/view/filter.js
+++ src/view/filter.js
@@ -1,7 +1,7 @@
 import AbstractView from "./abstract.js";
 
-const createFilterItemTemplate = (filter, isChecked) => {
-  const {name, count} = filter;
+const createFilterItemTemplate = (filter, currentFilterType) => {
+  const {type, name, count} = filter;
 
   return (
     `<input
@@ -9,8 +9,9 @@ const createFilterItemTemplate = (filter, isChecked) => {
       id="filter__${name}"
       class="filter__input visually-hidden"
       name="filter"
-      ${isChecked ? `checked` : ``}
+      ${type === currentFilterType ? `checked` : ``}
       ${count === 0 ? `disabled` : ``}
+      value="${type}"
     />
     <label for="filter__${name}" class="filter__label">
       ${name} <span class="filter__${name}-count">${count}</span></label
@@ -18,9 +19,9 @@ const createFilterItemTemplate = (filter, isChecked) => {
   );
 };
 
-export const createFilterTemplate = (filterItems) => {
+export const createFilterTemplate = (filterItems, currentFilterType) => {
   const filterItemsTemplate = filterItems
-    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
+    .map((filter) => createFilterItemTemplate(filter, currentFilterType))
     .join(``);
 
   return `<section class="main__filter filter container">
@@ -29,12 +30,25 @@ export const createFilterTemplate = (filterItems) => {
 };
 
 export default class Filter extends AbstractView {
-  constructor(filters) {
+  constructor(filters, currentFilterType) {
     super();
     this._filters = filters;
+    this._currentFilter = currentFilterType;
+
+    this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createFilterTemplate(this._filters);
+    return createFilterTemplate(this._filters, this._currentFilter);
+  }
+
+  _filterTypeChangeHandler(evt) {
+    evt.preventDefault();
+    this._callback.filterTypeChange(evt.target.value);
+  }
+
+  setFilterTypeChangeHandler(callback) {
+    this._callback.filterTypeChange = callback;
+    this.getElement().addEventListener(`change`, this._filterTypeChangeHandler);
   }
 }
