dec960434cc86f1ee93a5446b6b9e5f2e3188134
Sasha Sushko <sushko@outlook.com>
2020-08-21 16:04:42 +0300

    7.2.5 Установит пакеты для графиков. WIP
    - Установим chart.js и плагин к нему
    - Заведем в представлении статистики две функции, которые должны будут
      отрисовывать два графика (пока пустые)
    - Покажем в шаблоне счетчик выполненных задач за выбранный период
    
    WIP: линтер ругается на неиспользуемые переменные

---
+++ src/utils/statistics.js
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+
+dayjs.extend(isBetween);
+
+export const countCompletedTaskInDateRange = (tasks, dateFrom, dateTo) =>
+  tasks.reduce((counter, task) => {
+    if (task.dueDate === null) {
+      return counter;
+    }
+
+    // С помощью day.js проверям, сколько задач с дедлайном
+    // попадают в диапазон дат
+    if (
+      dayjs(task.dueDate).isSame(dateFrom) ||
+      dayjs(task.dueDate).isBetween(dateFrom, dateTo) ||
+      dayjs(task.dueDate).isSame(dateTo)
+    ) {
+      return counter + 1;
+    }
+
+    return counter;
+  }, 0);
