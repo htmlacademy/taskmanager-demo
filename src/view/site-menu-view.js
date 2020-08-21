440b7d009c01057de0bf641558435dc29abf584f
Sasha Sushko <sushko@outlook.com>
2020-08-21 11:46:39 +0300

    7.2.1 Запрограммирует меню приложения
    - Научим представление меню учитывать выбранный пункт
    - Добавим подписку для обработчика перехода
    - Опишем обработчик перехода (пока пустой)

--- src/view/site-menu-view.js
+++ src/view/site-menu-view.js
@@ -1,4 +1,5 @@
 import AbstractView from './abstract-view.js';
+import {MenuItem} from '../const.js';
 
 // Функцию для генерации HTML-разметки можно превратить в метод класса,
 // однако делать мы этого не будем, чтобы не раздувать diff изменений
@@ -9,6 +10,7 @@ const createSiteMenuTemplate = () => (
       name="control"
       id="control__new-task"
       class="control__input visually-hidden"
+      value="${MenuItem.ADD_NEW_TASK}"
     />
     <label for="control__new-task" class="control__label control__label--new-task"
       >+ ADD NEW TASK</label
@@ -18,6 +20,7 @@ const createSiteMenuTemplate = () => (
       name="control"
       id="control__task"
       class="control__input visually-hidden"
+      value="${MenuItem.TASKS}"
       checked
     />
     <label for="control__task" class="control__label">TASKS</label>
@@ -26,6 +29,7 @@ const createSiteMenuTemplate = () => (
       name="control"
       id="control__statistic"
       class="control__input visually-hidden"
+      value="${MenuItem.STATISTICS}"
     />
     <label for="control__statistic" class="control__label"
       >STATISTICS</label
@@ -36,5 +40,23 @@ const createSiteMenuTemplate = () => (
 export default class SiteMenuView extends AbstractView {
   get template() {
     return createSiteMenuTemplate();
+  }
+
+  setMenuClickHandler = (callback) => {
+    this._callback.menuClick = callback;
+    this.element.addEventListener('change', this.#menuClickHandler);
+  }
+
+  setMenuItem = (menuItem) => {
+    const item = this.element.querySelector(`[value=${menuItem}]`);
+
+    if (item !== null) {
+      item.checked = true;
+    }
+  }
+
+  #menuClickHandler = (evt) => {
+    evt.preventDefault();
+    this._callback.menuClick(evt.target.value);
   }
 }
