e84d04e85c0375dc7939b79e6ad853cf3da8ff45
Sasha Sushko <sushko@outlook.com>
2020-08-21 16:11:04 +0300

    7.2.6 Добавляем код графиков от коллеги
    - Добавим код chart.js графиков
    - Отметим в коде коллеги, что необходимо изменить

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -7,10 +7,123 @@ import {countCompletedTaskInDateRange} from "../utils/statistics.js";
 
 const renderColorsChart = (colorsCtx, tasks) => {
   // Функция для отрисовки графика по цветам
+  return new Chart(colorsCtx, {
+    plugins: [ChartDataLabels],
+    type: `pie`,
+    data: {
+      labels: [`BLACK`], // Сюда нужно передать названия уникальных цветов, они станут ярлыками
+      datasets: [{
+        data: [1], // Сюда нужно передать в том же порядке количество задач по каждому цвету
+        backgroundColor: [`#000`] // Сюда нужно передать в том же порядке HEX каждого цвета
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          display: false
+        }
+      },
+      tooltips: {
+        callbacks: {
+          label: (tooltipItem, data) => {
+            const allData = data.datasets[tooltipItem.datasetIndex].data;
+            const tooltipData = allData[tooltipItem.index];
+            const total = allData.reduce((acc, it) => acc + parseFloat(it));
+            const tooltipPercentage = Math.round((tooltipData / total) * 100);
+            return `${tooltipData} TASKS — ${tooltipPercentage}%`;
+          }
+        },
+        displayColors: false,
+        backgroundColor: `#ffffff`,
+        bodyFontColor: `#000000`,
+        borderColor: `#000000`,
+        borderWidth: 1,
+        cornerRadius: 0,
+        xPadding: 15,
+        yPadding: 15
+      },
+      title: {
+        display: true,
+        text: `DONE BY: COLORS`,
+        fontSize: 16,
+        fontColor: `#000000`
+      },
+      legend: {
+        position: `left`,
+        labels: {
+          boxWidth: 15,
+          padding: 25,
+          fontStyle: 500,
+          fontColor: `#000000`,
+          fontSize: 13
+        }
+      }
+    }
+  });
 };
 
 const renderDaysChart = (daysCtx, tasks, dateFrom, dateTo) => {
   // Функция для отрисовки графика по датам
+  return new Chart(daysCtx, {
+    plugins: [ChartDataLabels],
+    type: `line`,
+    data: {
+      labels: [`3 Sep`], // Сюда нужно передать названия дней
+      datasets: [{
+        data: [1], // Сюда нужно передать в том же порядке количество задач по каждому дню
+        backgroundColor: `transparent`,
+        borderColor: `#000000`,
+        borderWidth: 1,
+        lineTension: 0,
+        pointRadius: 8,
+        pointHoverRadius: 8,
+        pointBackgroundColor: `#000000`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 8
+          },
+          color: `#ffffff`
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            display: false
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            fontStyle: `bold`,
+            fontColor: `#000000`
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: {
+          top: 10
+        }
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  });
 };
 
 const createStatisticsTemplate = (data) => {
