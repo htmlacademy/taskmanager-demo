9741af899718f8b54eea8ed1cb0c8adc65652660
Sasha Sushko <sushko@outlook.com>
2020-08-21 11:46:39 +0300

    7.2.1 Программируем меню приложения
    - Научим представление меню учитывать выбранный пункт
    - Добавим подписку для обработчика перехода
    - Опишем обработчик перехода (пока пустой)

--- src/const.js
+++ src/const.js
@@ -26,3 +26,9 @@ export const FilterType = {
   REPEATING: `repeating`,
   ARCHIVE: `archive`
 };
+
+export const MenuItem = {
+  ADD_NEW_TASK: `ADD_NEW_TASK`,
+  TASKS: `TASKS`,
+  STATISTICS: `STATISTICS`
+};
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,4 +1,5 @@
 import AbstractView from "./abstract.js";
+import {MenuItem} from "../const.js";
 
 // Функцию для генерации HTML-разметки можно превратить в метод класса,
 // однако делать мы этого не будем, чтобы не раздувать diff изменений
@@ -9,6 +10,7 @@ const createSiteMenuTemplate = () => {
       name="control"
       id="control__new-task"
       class="control__input visually-hidden"
+      value="${MenuItem.ADD_NEW_TASK}"
     />
     <label for="control__new-task" class="control__label control__label--new-task"
       >+ ADD NEW TASK</label
@@ -18,6 +20,7 @@ const createSiteMenuTemplate = () => {
       name="control"
       id="control__task"
       class="control__input visually-hidden"
+      value="${MenuItem.TASKS}"
       checked
     />
     <label for="control__task" class="control__label">TASKS</label>
@@ -26,6 +29,7 @@ const createSiteMenuTemplate = () => {
       name="control"
       id="control__statistic"
       class="control__input visually-hidden"
+      value="${MenuItem.STATISTICS}"
     />
     <label for="control__statistic" class="control__label"
       >STATISTICS</label
@@ -34,7 +38,31 @@ const createSiteMenuTemplate = () => {
 };
 
 export default class SiteMenu extends AbstractView {
+  constructor() {
+    super();
+
+    this._menuClickHandler = this._menuClickHandler.bind(this);
+  }
+
   getTemplate() {
     return createSiteMenuTemplate();
+  }
+
+  _menuClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.menuClick(evt.target.value);
+  }
+
+  setMenuClickHandler(callback) {
+    this._callback.menuClick = callback;
+    this.getElement().addEventListener(`change`, this._menuClickHandler);
+  }
+
+  setMenuItem(menuItem) {
+    const item = this.getElement().querySelector(`[value=${menuItem}]`);
+
+    if (item !== null) {
+      item.checked = true;
+    }
   }
 }
