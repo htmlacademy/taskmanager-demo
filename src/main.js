93a51de299730bc686b35c4e47ef015015795b85
Sasha Sushko <sushko@outlook.com>
2020-08-21 12:20:17 +0300

    7.2.3 Реализуем скрытие и показ доски
    - Добавим презентеру доски метод destroy
    - Перенесем подписку на модель из конструктора в метод инициализации.
      Это нужно для того, чтобы при destroy отписаться от моделей, а при
      повторной инициализации подписаться
    - Унесем в обработчик переключения меню логику из createTask по сбросу
      фильтрации, а сортировка будет теперь сама сбрасываться при
      destroy/init презентера доски

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
-import {MenuItem} from "./const.js";
+import {MenuItem, UpdateType, FilterType} from "./const.js";
 
 const TASK_COUNT = 22;
 
@@ -34,16 +34,18 @@ const handleSiteMenuClick = (menuItem) => {
   switch (menuItem) {
     case MenuItem.ADD_NEW_TASK:
       // Скрыть статистику
-      // Показать доску
+      boardPresenter.destroy();
+      filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
+      boardPresenter.init();
       boardPresenter.createTask(handleTaskNewFormClose);
       siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
       break;
     case MenuItem.TASKS:
-      // Показать доску
+      boardPresenter.init();
       // Скрыть статистику
       break;
     case MenuItem.STATISTICS:
-      // Скрыть доску
+      boardPresenter.destroy();
       // Показать статистику
       break;
   }
--- src/presenter/board.js
+++ src/presenter/board.js
@@ -8,7 +8,7 @@ import TaskNewPresenter from "./task-new.js";
 import {render, RenderPosition, remove} from "../utils/render.js";
 import {sortTaskUp, sortTaskDown} from "../utils/task.js";
 import {filter} from "../utils/filter.js";
-import {SortType, UpdateType, UserAction, FilterType} from "../const.js";
+import {SortType, UpdateType, UserAction} from "../const.js";
 
 
 const TASK_COUNT_PER_STEP = 8;
@@ -35,9 +35,6 @@ export default class Board {
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
     this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
 
-    this._tasksModel.addObserver(this._handleModelEvent);
-    this._filterModel.addObserver(this._handleModelEvent);
-
     this._taskNewPresenter = new TaskNewPresenter(this._taskListComponent, this._handleViewAction);
   }
 
@@ -45,12 +42,23 @@ export default class Board {
     render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
 
+    this._tasksModel.addObserver(this._handleModelEvent);
+    this._filterModel.addObserver(this._handleModelEvent);
+
     this._renderBoard();
   }
 
+  destroy() {
+    this._clearBoard({resetRenderedTaskCount: true, resetSortType: true});
+
+    remove(this._taskListComponent);
+    remove(this._boardComponent);
+
+    this._tasksModel.removeObserver(this._handleModelEvent);
+    this._filterModel.removeObserver(this._handleModelEvent);
+  }
+
   createTask(callback) {
-    this._currentSortType = SortType.DEFAULT;
-    this._filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
     this._taskNewPresenter.init(callback);
   }
 
