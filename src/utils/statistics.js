40ffd6488ab80dd1d6a1c16e365544ebb87774f9
Sasha Sushko <sushko@outlook.com>
2020-08-21 16:23:43 +0300

    7.2.7 Настраиваем график по цветам
    - Опишем вспомогательные функции для подсчета
    - Посчитаем и передадим в chart.js готовые данные

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -1,7 +1,23 @@
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
+import {Color} from "../const.js";
 
 dayjs.extend(isBetween);
+
+export const colorToHex = {
+  [Color.BLACK]: `#000000`,
+  [Color.BLUE]: `#0c5cdd`,
+  [Color.GREEN]: `#31b55c`,
+  [Color.PINK]: `#ff3cb9`,
+  [Color.YELLOW]: `#ffe125`
+};
+
+// Используем особенности Set, чтобы удалить дубли в массиве
+export const makeItemsUniq = (items) => [...new Set(items)];
+
+export const countTasksByColor = (tasks, color) => {
+  return tasks.filter((task) => task.color === color).length;
+};
 
 export const countCompletedTaskInDateRange = (tasks, dateFrom, dateTo) => {
   return tasks.reduce((counter, task) => {
