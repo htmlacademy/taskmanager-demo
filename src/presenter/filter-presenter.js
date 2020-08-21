dbe6ee6aa076f62509fa56594cfa43873a6f5caf
Sasha Sushko <sushko@outlook.com>
2020-08-21 12:20:17 +0300

    7.2.4 Реализует скрытие и показ фильтров
    - Добавим презентеру фильтра метод destroy
    - Перенесём подписку на модели из конструктора в метод инициализации
    - Перенесём из createTask в презентер фильтра логику по сбросу
      фильтрации

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -14,9 +14,6 @@ export default class FilterPresenter {
     this.#filterContainer = filterContainer;
     this.#filterModel = filterModel;
     this.#tasksModel = tasksModel;
-
-    this.#tasksModel.addObserver(this.#handleModelEvent);
-    this.#filterModel.addObserver(this.#handleModelEvent);
   }
 
   get filters() {
@@ -63,6 +60,9 @@ export default class FilterPresenter {
     this.#filterComponent = new FilterView(filters, this.#filterModel.filter);
     this.#filterComponent.setFilterTypeChangeHandler(this.#handleFilterTypeChange);
 
+    this.#tasksModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
+
     if (prevFilterComponent === null) {
       render(this.#filterContainer, this.#filterComponent, RenderPosition.BEFOREEND);
       return;
@@ -70,6 +70,16 @@ export default class FilterPresenter {
 
     replace(this.#filterComponent, prevFilterComponent);
     remove(prevFilterComponent);
+  }
+
+  destroy = () => {
+    remove(this.#filterComponent);
+    this.#filterComponent = null;
+
+    this.#tasksModel.removeObserver(this.#handleModelEvent);
+    this.#filterModel.removeObserver(this.#handleModelEvent);
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
   }
 
   #handleModelEvent = () => {
