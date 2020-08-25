6302207ac7eb9e038a8555980e1b4368621f2ca5
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:51:36 +0300

    8.1.5 Обучаем модель уведомлять о новых данных. WIP
    - Заведем новый тип события - init - он потребуется только один раз, при инициализации приложения
    
    WIP: данные приходят, но не отображаются, потому наблюдатели за моделью не обрабатывают событие init

--- src/const.js
+++ src/const.js
@@ -23,7 +23,8 @@ export const UserAction = {
 export const UpdateType = {
   PATCH: `PATCH`,
   MINOR: `MINOR`,
-  MAJOR: `MAJOR`
+  MAJOR: `MAJOR`,
+  INIT: `INIT`
 };
 
 export const FilterType = {
