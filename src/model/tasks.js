bac3fb7b29de42668a34df69d0698d582fa7d00c
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:51:36 +0300

    8.1.5 Обучаем модель уведомлять о новых данных. WIP
    - Заведем новый тип события - init - он потребуется только один раз, при инициализации приложения
    
    WIP: данные приходят, но не отображаются, потому наблюдатели за моделью не обрабатывают событие init

--- src/model/tasks.js
+++ src/model/tasks.js
@@ -6,8 +6,10 @@ export default class Tasks extends Observer {
     this._tasks = [];
   }
 
-  setTasks(tasks) {
+  setTasks(updateType, tasks) {
     this._tasks = tasks.slice();
+
+    this._notify(updateType);
   }
 
   getTasks() {
