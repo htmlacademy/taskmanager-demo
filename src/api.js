9800628abce8676585201da71e0f99069f03d427
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:05:22 +0300

    8.1.1 Создаем класс для работы с API

---
+++ src/api.js
@@ -0,0 +1,66 @@
+const Method = {
+  GET: `GET`,
+  PUT: `PUT`
+};
+
+const SuccessHTTPStatusRange = {
+  MIN: 200,
+  MAX: 299
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getTasks() {
+    return this._load({url: `tasks`})
+      .then(Api.toJSON);
+  }
+
+  updateTask(task) {
+    return this._load({
+      url: `tasks/${task.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(task),
+      headers: new Headers({"Content-Type": `application/json`})
+    })
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers()
+  }) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(
+        `${this._endPoint}/${url}`,
+        {method, body, headers}
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static checkStatus(response) {
+    if (
+      response.status < SuccessHTTPStatusRange.MIN &&
+      response.status > SuccessHTTPStatusRange.MAX
+    ) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response;
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+
+  static catchError(err) {
+    throw err;
+  }
+}
