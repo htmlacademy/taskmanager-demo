f184f5ad3e68a4e5b990339e5cda8bcd057ab46e
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:14:10 +0300

    8.1.2 Добавит модуль TasksApiService в точке входа. WIP
    - Добавим импорт модуля
    - Создадим экземпляр, передадим его в модель задач и попробуем получить задачи с сервера
    - Выведем результат в консоль
    
    WIP: линтер ругается на консоль

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import BoardPresenter from './presenter/board-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import TasksModel from './model/tasks-model.js';
 import FilterModel from './model/filter-model.js';
+import TasksApiService from './tasks-api-service.js';
+
+const AUTHORIZATION = 'Basic hS2sfS44wcl1sa2j';
+const END_POINT = 'https://21.objects.pages.academy/task-manager';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
-const tasksModel = new TasksModel();
+
+const tasksModel = new TasksModel({
+  tasksApiService: new TasksApiService(END_POINT, AUTHORIZATION)
+});
 const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter({
   boardContainer: siteMainElement,
