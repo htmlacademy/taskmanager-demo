225f958ab6449c2d787293a601ff112a4051a226
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:31:05 +0300

    8.1.3 Реализует паттерн "Адаптер". WIP
    - Добавим в модель метод для переделки структуры данных сервера в структуру данных клиента
    - Добавим в модуль ApiService метод для переделки структуры данных клиента в структуру данных сервера
    - Используем эти методы, чтобы переводить в серверную структуру данные перед обновлением задачи и в клиентскую структуру при получении списка задач
    
    WIP: линтер ругается на консоль

--- src/api-service.js
+++ src/api-service.js
@@ -21,7 +21,7 @@ export default class ApiService {
     const response = await this.#load({
       url: `tasks/${task.id}`,
       method: Method.PUT,
-      body: JSON.stringify(task),
+      body: JSON.stringify(this.#adaptToServer(task)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
@@ -49,6 +49,23 @@ export default class ApiService {
     } catch (err) {
       ApiService.catchError(err);
     }
+  }
+
+  #adaptToServer = (task) => {
+    const adaptedTask = {...task,
+      'due_date': task.dueDate instanceof Date ? task.dueDate.toISOString() : null, // На сервере дата хранится в ISO формате
+      'is_archived': task.isArchive,
+      'is_favorite': task.isFavorite,
+      'repeating_days': task.repeating,
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedTask.dueDate;
+    delete adaptedTask.isArchive;
+    delete adaptedTask.isFavorite;
+    delete adaptedTask.repeating;
+
+    return adaptedTask;
   }
 
   static parseResponse = (response) => response.json();
--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -9,11 +9,7 @@ export default class TasksModel extends AbstractObservable {
     this.#apiService = apiService;
 
     this.#apiService.tasks.then((tasks) => {
-      console.log(tasks);
-      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
-      // а ещё на сервере используется snake_case, а у нас camelCase.
-      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
-      // Есть вариант получше - паттерн "Адаптер"
+      console.log(tasks.map(this.#adaptToClient));
     });
   }
 
@@ -63,5 +59,22 @@ export default class TasksModel extends AbstractObservable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient = (task) => {
+    const adaptedTask = {...task,
+      dueDate: task['due_date'] !== null ? new Date(task['due_date']) : task['due_date'], // На клиенте дата хранится как экземпляр Date
+      isArchive: task['is_archived'],
+      isFavorite: task['is_favorite'],
+      repeating: task['repeating_days'],
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedTask['due_date'];
+    delete adaptedTask['is_archived'];
+    delete adaptedTask['is_favorite'];
+    delete adaptedTask['repeating_days'];
+
+    return adaptedTask;
   }
 }
