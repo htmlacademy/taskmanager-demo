964889d082333b4d9631235c5251f46fc495369f
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:31:05 +0300

    8.1.3 Реализует паттерн "Адаптер". WIP
    - Добавим в модель метод для переделки структуры данных сервера в структуру данных клиента
    - Добавим в модуль TasksApiService метод для переделки структуры данных клиента в структуру данных сервера
    - Используем эти методы, чтобы переводить в серверную структуру данные перед обновлением задачи и в клиентскую структуру при получении списка задач
    
    WIP: линтер ругается на консоль

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -12,11 +12,7 @@ export default class TasksModel extends Observable {
     this.#tasksApiService = tasksApiService;
 
     this.#tasksApiService.tasks.then((tasks) => {
-      console.log(tasks);
-      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
-      // а ещё на сервере используется snake_case, а у нас camelCase.
-      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
-      // Есть вариант получше - паттерн "Адаптер"
+      console.log(tasks.map(this.#adaptToClient));
     });
   }
 
@@ -62,5 +58,22 @@ export default class TasksModel extends Observable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient(task) {
+    const adaptedTask = {...task,
+      dueDate: task['due_date'] !== null ? new Date(task['due_date']) : task['due_date'], // На клиенте дата хранится как экземпляр Date
+      isArchive: task['is_archived'],
+      isFavorite: task['is_favorite'],
+      repeating: task['repeating_days'],
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedTask['due_date'];
+    delete adaptedTask['is_archived'];
+    delete adaptedTask['is_favorite'];
+    delete adaptedTask['repeating_days'];
+
+    return adaptedTask;
   }
 }
--- src/tasks-api-service.js
+++ src/tasks-api-service.js
@@ -15,12 +15,29 @@ export default class TasksApiService extends ApiService {
     const response = await this._load({
       url: `tasks/${task.id}`,
       method: Method.PUT,
-      body: JSON.stringify(task),
+      body: JSON.stringify(this.#adaptToServer(task)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(task) {
+    const adaptedTask = {...task,
+      'due_date': task.dueDate instanceof Date ? task.dueDate.toISOString() : null, // На сервере дата хранится в ISO формате
+      'is_archived': task.isArchive,
+      'is_favorite': task.isFavorite,
+      'repeating_days': task.repeating,
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedTask.dueDate;
+    delete adaptedTask.isArchive;
+    delete adaptedTask.isFavorite;
+    delete adaptedTask.repeating;
+
+    return adaptedTask;
   }
 }
