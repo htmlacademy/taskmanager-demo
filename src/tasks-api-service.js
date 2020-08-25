01a18767a54fc546f3f02b1f37724153313a7499
Sasha Sushko <sushko@outlook.com>
2020-08-25 12:20:04 +0300

    8.2.1 "Включит" создание и удаление на сервере
    - Допишем в модуль TasksApiService методы для создания и удаления задач
    - Используем эти методы в модели задач
    - Удаляем генерацию id для новых задач на клиенте. Теперь генериацией id занимается сервер

--- src/tasks-api-service.js
+++ src/tasks-api-service.js
@@ -3,6 +3,8 @@ import ApiService from './framework/api-service.js';
 const Method = {
   GET: 'GET',
   PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
 };
 
 export default class TasksApiService extends ApiService {
@@ -22,6 +24,28 @@ export default class TasksApiService extends ApiService {
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  async addTask(task) {
+    const response = await this._load({
+      url: 'tasks',
+      method: Method.POST,
+      body: JSON.stringify(this.#adaptToServer(task)),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+
+  async deleteTask(task) {
+    const response = await this._load({
+      url: `tasks/${task.id}`,
+      method: Method.DELETE,
+    });
+
+    return response;
   }
 
   #adaptToServer(task) {
