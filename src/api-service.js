6828c047cc1764ede1797d5b638a83a51e44a6e9
Sasha Sushko <sushko@outlook.com>
2020-08-25 11:05:22 +0300

    8.1.1 Создаст класс для работы с API

---
+++ src/api-service.js
@@ -0,0 +1,65 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class ApiService {
+  #endPoint = null;
+  #authorization = null;
+
+  constructor(endPoint, authorization) {
+    this.#endPoint = endPoint;
+    this.#authorization = authorization;
+  }
+
+  get tasks() {
+    return this.#load({url: 'tasks'})
+      .then(ApiService.parseResponse);
+  }
+
+  updateTask = async (task) => {
+    const response = await this.#load({
+      url: `tasks/${task.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(task),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+
+  #load = async ({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) => {
+    headers.append('Authorization', this.#authorization);
+
+    const response = await fetch(
+      `${this.#endPoint}/${url}`,
+      {method, body, headers},
+    );
+
+    try {
+      ApiService.checkStatus(response);
+      return response;
+    } catch (err) {
+      ApiService.catchError(err);
+    }
+  }
+
+  static parseResponse = (response) => response.json();
+
+  static checkStatus = (response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  }
+
+  static catchError = (err) => {
+    throw err;
+  }
+}
