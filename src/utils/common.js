c8b2e154c97ed30cbd1ac98d391fec5178d1d5fa
Sasha Sushko <sushko@outlook.com>
2020-08-30 13:43:52 +0300

    9.1.6 Подключит провайдер и хранилище

--- src/utils/common.js
+++ src/utils/common.js
@@ -6,3 +6,7 @@ export const getRandomInteger = (a = 0, b = 1) => {
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const isOnline = () => {
+  return window.navigator.onLine;
+};
