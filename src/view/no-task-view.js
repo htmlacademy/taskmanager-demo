76823b61536d91af5dc284d3e6b627a865f8eb22
Anna Levchenko <levallevchenko@gmail.com>
2021-07-19 17:07:36 +0300

    7.11 Реализует изменение компонента-заглушки
    Добавим изменение текста в компоненте NoTaskView в зависимости от выбранного фильтра. Будем показывать подходящий текст, если не останется ни одной задачи, соответствующей выбранному фильтру. Например, если мы выберем задачи на сегодня и перенесём все задачи на другой день, в отфильтрованном списке появится заглушка «There are no tasks today».

--- src/view/no-task-view.js
+++ src/view/no-task-view.js
@@ -1,15 +1,33 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {FilterType} from '../const.js';
 
-function createNoTaskTemplate() {
+const NoTasksTextType = {
+  [FilterType.ALL]: 'Click «ADD NEW TASK» in menu to create your first task',
+  [FilterType.OVERDUE]: 'There are no overdue tasks now',
+  [FilterType.TODAY]: 'There are no tasks today',
+  [FilterType.FAVORITES]: 'There are no favorite tasks now',
+  [FilterType.REPEATING]: 'There are no repeating tasks now',
+  [FilterType.ARCHIVE]: 'There are no archive tasks now',
+};
+
+function createNoTaskTemplate(filterType) {
+  const noTaskTextValue = NoTasksTextType[filterType];
+
   return (
     `<p class="board__no-tasks">
-      Click «ADD NEW TASK» in menu to create your first task
+      ${noTaskTextValue}
     </p>`
   );
 }
-
 export default class NoTaskView extends AbstractView {
+  #filterType = null;
+
+  constructor({filterType}) {
+    super();
+    this.#filterType = filterType;
+  }
+
   get template() {
-    return createNoTaskTemplate();
+    return createNoTaskTemplate(this.#filterType);
   }
 }
