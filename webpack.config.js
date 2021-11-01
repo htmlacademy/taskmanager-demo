f48a44d3dc8abcc9126ab09dea21592aa7884c4e
Lydia Zakharova <jahontis@gmail.com>
2021-11-01 14:14:19 +0300

    1.3 Установит и настроит Babel
    - Установим основной пакет Babel и пресет
    - Опишем настройки Babel в отдельном файле
    - Установим лоадер для Вебпака, чтобы сборщик запускал Babel при сборке
    - Добавим лоадер в настройки Вебпака
    - Добавим в package.json настройки browserlist

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,13 @@ module.exports = {
       patterns: [{ from: 'public' }],
     }),
   ],
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        }
+    ]
+  }
 };
