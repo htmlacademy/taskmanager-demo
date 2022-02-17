6d925b0525a0c942357e245dfa40c6684431ba9c
Lydia Zakharova <jahontis@gmail.com>
2022-02-17 11:28:58 +0300

    2.3 Подключит модель
    - Импортируем TasksModel в точку входа и создадим экземпляр класса
    - Передадим экземпляр модели в презентер
    - Научим презентер получать задачи из модели и передавать их в представление

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import {render} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
+import TasksModel from './model/tasks-model.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
+
+const tasksModel = new TasksModel();
 const boardPresenter = new BoardPresenter();
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 
-boardPresenter.init(siteMainElement);
+boardPresenter.init(siteMainElement, tasksModel);
