9c2739c05b75553c21c6fccb1c749d50f19e8e7b
Lydia Zakharova <jahontis@gmail.com>
2022-04-25 09:06:27 +0300

    1.7 Добавит презентер доски
    - Создадим презентер - модуль, который вберёт в себя логику по работе с доской
    - Импортируем презентер в точку входа и инициализируем его

---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,29 @@
+import BoardView from '../view/board-view.js';
+import SortView from '../view/sort-view.js';
+import TaskListView from '../view/task-list-view.js';
+import TaskView from '../view/task-view.js';
+import TaskEditView from '../view/task-edit-view.js';
+import LoadMoreButtonView from '../view/load-more-button-view.js';
+import {render} from '../render.js';
+
+export default class BoardPresenter {
+  boardComponent = new BoardView();
+  taskListComponent = new TaskListView();
+
+  constructor({boardContainer}) {
+    this.boardContainer = boardContainer;
+  }
+
+  init() {
+    render(this.boardComponent, this.boardContainer);
+    render(new SortView(), this.boardComponent.getElement());
+    render(this.taskListComponent, this.boardComponent.getElement());
+    render(new TaskEditView(), this.taskListComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new TaskView(), this.taskListComponent.getElement());
+    }
+
+    render(new LoadMoreButtonView(), this.boardComponent.getElement());
+  }
+}
