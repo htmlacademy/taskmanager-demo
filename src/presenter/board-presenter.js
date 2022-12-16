Shapovaloff/taskmanager-17

3.1.4 Добавит возможность закрыть форму
- Объявим обработчик клавиши Esc, который будет закрывать форму
- Опишем логику добавления и удаления этого обработчика

src/presenter/board-presenter.js
@@ -43,13 +43,23 @@ export default class BoardPresenter {
       this.#taskListComponent.element.replaceChild(taskComponent.element, taskEditComponent.element);
     };
 
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
     taskComponent.element.querySelector('.card__btn--edit').addEventListener('click', () => {
       replaceCardToForm();
+      document.addEventListener('keydown', onEscKeyDown);
     });
 
     taskEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
       evt.preventDefault();
       replaceFormToCard();
+      document.removeEventListener('keydown', onEscKeyDown);
     });
 
     render(taskComponent, this.#taskListComponent.element);

htmlacademy/taskmanager-demo

src/presenter/board-presenter.js
@@ -46,13 +46,23 @@ export default class BoardPresenter {
       this.#taskListComponent.element.replaceChild(taskComponent.element, taskEditComponent.element);
     };
 
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
+
     taskComponent.element.querySelector('.card__btn--edit').addEventListener('click', () => {
       replaceCardToForm();
+      document.addEventListener('keydown', escKeyDownHandler);
     });
 
     taskEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
       evt.preventDefault();
       replaceFormToCard();
+      document.removeEventListener('keydown', escKeyDownHandler);
     });
 
     render(taskComponent, this.#taskListComponent.element);