6394df87216de24af1d778dac89a51b9ea8ab85f
Lydia Zakharova <jahontis@gmail.com>
2023-03-29 10:34:52 +0300

    1.5 Добавит хэширование
    - Добавим хэш к имени бандла
    - Удалим подключение `bundle.js` в `index.html`
    - Установим и настроим HtmlPlugin для автоматической генерации `index.html` при сборке
    - Добавим `index.html` в исключения для CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
     }),
   ],
   module: {
