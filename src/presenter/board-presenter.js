f228b02b14da4f0d8711eb6a3b88d7d8dff3807e
Lydia Zakharova <jahontis@gmail.com>
2023-04-07 10:53:45 +0300

    4.1.4 Добавит классам приватные поля и геттеры
    Сделаем часть полей в классах приватными

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -7,26 +7,31 @@ import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  taskListComponent = new TaskListView();
+  #boardContainer = null;
+  #tasksModel = null;
+
+  #boardComponent = new BoardView();
+  #taskListComponent = new TaskListView();
+
+  #boardTasks = [];
 
   constructor({boardContainer, tasksModel}) {
-    this.boardContainer = boardContainer;
-    this.tasksModel = tasksModel;
+    this.#boardContainer = boardContainer;
+    this.#tasksModel = tasksModel;
   }
 
   init() {
-    this.boardTasks = [...this.tasksModel.getTasks()];
+    this.#boardTasks = [...this.#tasksModel.tasks];
 
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.element);
-    render(this.taskListComponent, this.boardComponent.element);
-    render(new TaskEditView({task: this.boardTasks[0]}), this.taskListComponent.element);
+    render(this.#boardComponent, this.#boardContainer);
+    render(new SortView(), this.#boardComponent.element);
+    render(this.#taskListComponent, this.#boardComponent.element);
+    render(new TaskEditView({task: this.#boardTasks[0]}), this.#taskListComponent.element);
 
-    for (let i = 1; i < this.boardTasks.length; i++) {
-      render(new TaskView({task: this.boardTasks[i]}), this.taskListComponent.element);
+    for (let i = 1; i < this.#boardTasks.length; i++) {
+      render(new TaskView({task: this.#boardTasks[i]}), this.#taskListComponent.element);
     }
 
-    render(new LoadMoreButtonView(), this.boardComponent.element);
+    render(new LoadMoreButtonView(), this.#boardComponent.element);
   }
 }
