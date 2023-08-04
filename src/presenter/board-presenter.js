wonderjs/taskmanager-18

7.5 Доработает рендеринг доски
- Опишем новый метод #clearBoard для очистки доски: он умеет очищать список и (по необходимости) сбрасывать количество показанных задач или сортировку
- #renderBoard объединим с #renderTaskList и тоже научим отрисовывать не просто TASK_COUNT_PER_STEP задач, а столько - сколько было до перерисовки
- методы отрисовки сортировки и кнопки допоказа рефакторим, чтобы они умели перерисовываться вместе с доской
- Шаблон сортировки научим учитывать выбранную и отмечать активный пункт. Теперь это возможно, потому что компонент сортировки перерисовывается с доской

src/presenter/board-presenter.js
@@ -16,9 +16,9 @@ export default class BoardPresenter {
 
   #boardComponent = new BoardView();
   #taskListComponent = new TaskListView();
-  #sortComponent = new SortView();
   #noTaskComponent = new NoTaskView();
-  #loadMoreButtonComponent = new LoadMoreButtonView();
+  #sortComponent = null;
+  #loadMoreButtonComponent = null;
 
   #renderedTaskCount = TASK_COUNT_PER_STEP;
   #taskPresenter = new Map();
@@ -78,18 +78,17 @@ export default class BoardPresenter {
   };
 
   #handleModelEvent = (updateType, data) => {
-    console.log(updateType, data);
-    // В зависимости от типа изменений решаем, что делать:
     switch (updateType) {
       case UpdateType.PATCH:
-        // - обновить часть списка (например, когда поменялось описание)
         this.#taskPresenter.get(data.id).init(data);
         break;
       case UpdateType.MINOR:
-        // - обновить список (например, когда задача ушла в архив)
+        this.#clearBoard();
+        this.#renderBoard();
         break;
       case UpdateType.MAJOR:
-        // - обновить всю доску (например, при переключении фильтра)
+        this.#clearBoard({resetRenderedTaskCount: true, resetSortType: true});
+        this.#renderBoard();
         break;
     }
   };
@@ -100,13 +99,15 @@ export default class BoardPresenter {
     }
 
     this.#currentSortType = sortType;
-    this.#clearTaskList();
-    this.#renderTaskList();
+    this.#clearBoard({resetRenderedTaskCount: true});
+    this.#renderBoard();
   };
 
   #renderSort = () => {
-    render(this.#sortComponent, this.#boardComponent.element, RenderPosition.AFTERBEGIN);
+    this.#sortComponent = new SortView(this.#currentSortType);
     this.#sortComponent.setSortTypeChangeHandler(this.#handleSortTypeChange);
+
+    render(this.#sortComponent, this.#boardComponent.element, RenderPosition.AFTERBEGIN);
   };
 
   #renderTask = (task) => {
@@ -124,9 +125,10 @@ export default class BoardPresenter {
   };
 
   #renderLoadMoreButton = () => {
-    render(this.#loadMoreButtonComponent, this.#boardComponent.element);
-
+    this.#loadMoreButtonComponent = new LoadMoreButtonView();
     this.#loadMoreButtonComponent.setClickHandler(this.#handleLoadMoreButtonClick);
+
+    render(this.#loadMoreButtonComponent, this.#boardComponent.element);
   };
 
   #clearTaskList = () => {
@@ -148,14 +150,52 @@ export default class BoardPresenter {
     }
   };
 
+  #clearBoard = ({resetRenderedTaskCount = false, resetSortType = false} = {}) => {
+    const taskCount = this.tasks.length;
+
+    this.#taskPresenter.forEach((presenter) => presenter.destroy());
+    this.#taskPresenter.clear();
+
+    remove(this.#sortComponent);
+    remove(this.#noTaskComponent);
+    remove(this.#loadMoreButtonComponent);
+
+    if (resetRenderedTaskCount) {
+      this.#renderedTaskCount = TASK_COUNT_PER_STEP;
+    } else {
+      // На случай, если перерисовка доски вызвана
+      // уменьшением количества задач (например, удаление или перенос в архив)
+      // нужно скорректировать число показанных задач
+      this.#renderedTaskCount = Math.min(taskCount, this.#renderedTaskCount);
+    }
+
+    if (resetSortType) {
+      this.#currentSortType = SortType.DEFAULT;
+    }
+  };
+
   #renderBoard = () => {
+    const tasks = this.tasks;
+    const taskCount = tasks.length;
+
     render(this.#boardComponent, this.#boardContainer);
-    if (this.tasks.every((task) => task.isArchive)) {
+
+    if (taskCount === 0) {
       this.#renderNoTasks();
       return;
     }
 
     this.#renderSort();
-    this.#renderTaskList();
+    render(this.#taskListComponent, this.#boardComponent.element);
+
+    // Теперь, когда #renderBoard рендерит доску не только на старте,
+    // но и по ходу работы приложения, нужно заменить
+    // константу TASK_COUNT_PER_STEP на свойство #renderedTaskCount,
+    // чтобы в случае перерисовки сохранить N-показанных карточек
+    this.#renderTasks(tasks.slice(0, Math.min(taskCount, this.#renderedTaskCount)));
+
+    if (taskCount > this.#renderedTaskCount) {
+      this.#renderLoadMoreButton();
+    }
   };
 }

htmlacademy/taskmanager-demo

src/presenter/board-presenter.js
@@ -78,18 +78,17 @@ export default class BoardPresenter {
   };
 
   #handleModelEvent = (updateType, data) => {
-    console.log(updateType, data);
-    // В зависимости от типа изменений решаем, что делать:
     switch (updateType) {
       case UpdateType.PATCH:
-        // - обновить часть списка (например, когда поменялось описание)
         this.#taskPresenters.get(data.id).init(data);
         break;
       case UpdateType.MINOR:
-        // - обновить список (например, когда задача ушла в архив)
+        this.#clearBoard();
+        this.#renderBoard();
         break;
       case UpdateType.MAJOR:
-        // - обновить всю доску (например, при переключении фильтра)
+        this.#clearBoard({resetRenderedTaskCount: true, resetSortType: true});
+        this.#renderBoard();
         break;
     }
   };
@@ -100,12 +99,13 @@ export default class BoardPresenter {
     }
 
     this.#currentSortType = sortType;
-    this.#clearTaskList();
-    this.#renderTaskList();
+    this.#clearBoard({resetRenderedTaskCount: true});
+    this.#renderBoard();
   };
 
   #renderSort() {
     this.#sortComponent = new SortView({
+      currentSortType: this.#currentSortType,
       onSortTypeChange: this.#handleSortTypeChange
     });
 
@@ -157,14 +157,52 @@ export default class BoardPresenter {
     }
   }
 
+  #clearBoard({resetRenderedTaskCount = false, resetSortType = false} = {}) {
+    const taskCount = this.tasks.length;
+
+    this.#taskPresenters.forEach((presenter) => presenter.destroy());
+    this.#taskPresenters.clear();
+
+    remove(this.#sortComponent);
+    remove(this.#noTaskComponent);
+    remove(this.#loadMoreButtonComponent);
+
+    if (resetRenderedTaskCount) {
+      this.#renderedTaskCount = TASK_COUNT_PER_STEP;
+    } else {
+      // На случай, если перерисовка доски вызвана
+      // уменьшением количества задач (например, удаление или перенос в архив)
+      // нужно скорректировать число показанных задач
+      this.#renderedTaskCount = Math.min(taskCount, this.#renderedTaskCount);
+    }
+
+    if (resetSortType) {
+      this.#currentSortType = SortType.DEFAULT;
+    }
+  }
+
   #renderBoard() {
     render(this.#boardComponent, this.#boardContainer);
-    if (this.tasks.every((task) => task.isArchive)) {
+
+    const tasks = this.tasks;
+    const taskCount = tasks.length;
+
+    if (taskCount === 0) {
       this.#renderNoTasks();
       return;
     }
 
     this.#renderSort();
-    this.#renderTaskList();
+    render(this.#taskListComponent, this.#boardComponent.element);
+
+    // Теперь, когда #renderBoard рендерит доску не только на старте,
+    // но и по ходу работы приложения, нужно заменить
+    // константу TASK_COUNT_PER_STEP на свойство #renderedTaskCount,
+    // чтобы в случае перерисовки сохранить N-показанных карточек
+    this.#renderTasks(tasks.slice(0, Math.min(taskCount, this.#renderedTaskCount)));
+
+    if (taskCount > this.#renderedTaskCount) {
+      this.#renderLoadMoreButton();
+    }
   }
 }